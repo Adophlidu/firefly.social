0297e71d36a8c5b3c460b40951eb3161f207e171
dudu0506 <dudu@mask.io>
2025-01-15 21:59:06 +0800

    fix: solana devnet rpc endpoint (#3746)

--- src/helpers/getSolanaRPCUrl.ts
+++ src/helpers/getSolanaRPCUrl.ts
@@ -3,8 +3,11 @@ import { web3 } from '@coral-xyz/anchor';
 import { STATUS } from '@/constants/enum.js';
 import { env } from '@/constants/env.js';
 
+const SOLANA_DEV_RPC_URL =
+    'https://chaotic-solemn-sound.solana-devnet.quiknode.pro/4fc40f8f7d6d57cdc6735ea81a39e07f1fdafc2e';
+
 export function getSolanaRPCUrl() {
     return env.external.NEXT_PUBLIC_SOLANA_DEV === STATUS.Enabled
-        ? web3.clusterApiUrl('devnet')
+        ? SOLANA_DEV_RPC_URL
         : env.external.NEXT_PUBLIC_SOLANA_RPC_URL || web3.clusterApiUrl('mainnet-beta');
 }
