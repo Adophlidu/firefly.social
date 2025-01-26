dcaaf6e2b734bdb48896636b5a7a19ce7982d4ed
Wukong Sun <swkatmask@gmail.com>
2025-01-26 22:16:11 +0800

    feat: bluesky login form (#3865)
    * feat: bluesky login form
    
    * chore: code maintenance [bot]
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- src/components/Login/LoginBsky.tsx
+++ src/components/Login/LoginBsky.tsx
@@ -2,25 +2,30 @@
 
 import { AtpAgent } from '@atproto/api';
 import { t } from '@lingui/core/macro';
+import { useState } from 'react';
 import { useAsyncFn } from 'react-use';
 
+import AtIcon from '@/assets/at.svg';
+import LockIcon from '@/assets/lock.svg';
 import { ClickableButton } from '@/components/ClickableButton.js';
+import { LoadingIcon } from '@/components/LoadingIcon.js';
 import { DEFAULT_SERVICE_URL } from '@/constants/bsky.js';
 import { Source } from '@/constants/enum.js';
-import { env } from '@/constants/env.js';
 import { enqueueSuccessMessage } from '@/helpers/enqueueMessage.js';
 import { resolveSourceName } from '@/helpers/resolveSourceName.js';
 import { LoginModalRef } from '@/modals/controls.js';
 
 export function LoginBsky() {
-    const [{ loading }, login] = useAsyncFn(async () => {
+    const [account, setAccount] = useState('');
+    const [password, setPassword] = useState('');
+    const [{ loading }, login] = useAsyncFn(async (account: string, password: string) => {
         const agent = new AtpAgent({
             service: DEFAULT_SERVICE_URL,
         });
 
         const response = await agent.login({
-            identifier: env.external.NEXT_PUBLIC_BSKY_IDENTIFIER ?? '',
-            password: env.external.NEXT_PUBLIC_BSKY_PASSWORD ?? '',
+            identifier: account,
+            password,
         });
 
         console.log('DEBUG: login response', response);
@@ -42,15 +47,43 @@ export function LoginBsky() {
     }, []);
 
     return (
-        <div>
-            <h1>Click the button below to login to Bluesky with the default account.</h1>
+        <form className="flex w-[400px] flex-col gap-3 p-6">
+            <h1 className="mx-2 text-lg">Enter your username and password</h1>
+
+            <div className="relative mx-0 flex h-10 flex-grow items-center rounded-xl bg-lightBg px-3 text-main focus-within:border-fireflyBrand">
+                <AtIcon width={18} height={18} className="shrink-0" />
+                <input
+                    type="text"
+                    name="account"
+                    autoComplete="off"
+                    spellCheck="false"
+                    placeholder={t`Username or email address`}
+                    className="w-full border-0 bg-transparent py-2 placeholder-secondary focus:border-0 focus:outline-0 focus:ring-0 dark:text-input sm:text-sm sm:leading-6"
+                    value={account}
+                    onChange={(ev) => setAccount(ev.currentTarget.value)}
+                />
+            </div>
+            <div className="relative mx-0 flex h-10 flex-grow items-center rounded-xl bg-lightBg px-3 text-main focus-within:border-fireflyBrand">
+                <LockIcon width={18} height={18} className="shrink-0" />
+                <input
+                    type="password"
+                    name="password"
+                    autoComplete="off"
+                    spellCheck="false"
+                    placeholder={t`Password`}
+                    className="w-full border-0 bg-transparent py-2 placeholder-secondary focus:border-0 focus:outline-0 focus:ring-0 dark:text-input sm:text-sm sm:leading-6"
+                    value={password}
+                    onChange={(ev) => setPassword(ev.currentTarget.value)}
+                />
+            </div>
             <ClickableButton
-                className="flex h-[42px] w-full items-center justify-center rounded-md border border-line"
-                disabled={loading}
-                onClick={login}
+                className="mt-1 flex h-[42px] w-full items-center justify-center rounded-md border border-line"
+                disabled={loading || !account || !password}
+                onClick={() => login(account, password)}
             >
+                {loading ? <LoadingIcon className="text-main" /> : null}
                 Login
             </ClickableButton>
-        </div>
+        </form>
     );
 }
