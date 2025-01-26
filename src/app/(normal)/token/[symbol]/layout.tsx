eebe5c5f92e3aa37f41d670d4928c1d795bae097
dudu0506 <dudu@mask.io>
2025-01-26 11:21:25 +0800

    fix: token symbol uppercase (#3852)

--- src/app/(normal)/token/[symbol]/layout.tsx
+++ src/app/(normal)/token/[symbol]/layout.tsx
@@ -32,7 +32,7 @@ export default async function TokenPageLayout(props: PropsWithChildren<Props>) {
             <div className="sticky top-0 z-30 flex h-[60px] items-center justify-between border-b border-line bg-primaryBottom px-4">
                 <div className="flex items-center gap-7">
                     <Comeback className="cursor-pointer text-lightMain" />
-                    <span className="text-xl font-black text-lightMain">${token?.symbol || paramSymbol}</span>
+                    <span className="text-xl font-black uppercase text-lightMain">${token?.symbol || paramSymbol}</span>
                 </div>
                 <SwapButton className="ml-auto sm:inline-flex md:hidden" />
             </div>
