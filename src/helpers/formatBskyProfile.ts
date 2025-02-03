acbf18573b4337441a87b3281a59520fbf27083a
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2025-02-03 23:10:20 +0800

    fix: get display name (#3896)
    * fix: get display name from handle
    
    * chore: code maintenance [bot]
    
    * chore: requirements
    
    * fix: condition
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- src/helpers/formatBskyProfile.ts
+++ src/helpers/formatBskyProfile.ts
@@ -1,14 +1,24 @@
 import type { AppBskyActorDefs } from '@atproto/api';
+import { first } from 'lodash-es';
 
 import { Source } from '@/constants/enum.js';
 import { type Profile, ProfileStatus } from '@/providers/types/SocialMedia.js';
+
+function getDisplayNameFromHandle(handle: string) {
+    // requirements:
+    // 1. doesn't begin or end with a hyphen
+    // 2. at least 3 characters
+    // 3. only contains letters, numbers, and hyphens
+    const matched = handle.match(/^(?!-)[A-Za-z0-9-]{3,}(?<!-)/) ?? [];
+    return first(matched) ?? handle;
+}
 
 export function formatBskyProfile(profile: AppBskyActorDefs.ProfileViewDetailed): Profile {
     return {
         profileId: profile.did,
         source: Source.Bsky,
         profileSource: Source.Bsky,
-        displayName: profile.displayName ?? profile.handle ?? '',
+        displayName: profile.displayName || getDisplayNameFromHandle(profile.handle),
         bio: profile.description ?? '',
         handle: profile.handle,
         fullHandle: profile.handle,
