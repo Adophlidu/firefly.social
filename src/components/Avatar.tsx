9e4dfd32ceb4806532c66433104177f96d44eae3
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2025-02-07 11:48:59 +0800

    refactor: Avatar (#3943)
    * refactor: Avatar
    
    * refactor: rm resolveImageUrl

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,19 +1,25 @@
 'use client';
 
-import { useQuery } from '@tanstack/react-query';
-import { compact } from 'lodash-es';
 import type { ImageProps as NextImageProps } from 'next/image.js';
 import { memo, useState } from 'react';
 
 import { Image as NextImage } from '@/esm/Image.js';
 import { classNames } from '@/helpers/classNames.js';
 import { isDomainOrSubdomainOf } from '@/helpers/isDomainOrSubdomainOf.js';
-import { resolveFirstAvailableUrl } from '@/helpers/resolveFirstAvailableUrl.js';
-import { resolveImgurUrl } from '@/helpers/resolveImgurUrl.js';
 import { useIsDarkMode } from '@/hooks/useIsDarkMode.js';
 
-function resolveAvatarFallbackUrl(url: string | undefined, isDarkMode = false) {
-    if (!url?.startsWith('https://cdn.stamp.fyi/avatar/eth:')) return url;
+function resolveImgurUrl(url: string) {
+    if (!URL.canParse(url)) return;
+
+    const u = new URL(url);
+    if (u.protocol !== 'https:') return;
+    if (u.host !== 'i.imgur.com') return;
+
+    return `https://res.cloudinary.com/merkle-manufactory/image/fetch/c_fill,f_jpg,w_144/${encodeURIComponent(url)}`;
+}
+
+function resolveAvatarFallbackUrl(url: string, isDarkMode = false) {
+    if (!url?.startsWith('https://cdn.stamp.fyi/avatar/eth:')) return;
     return isDarkMode ? '/image/firefly-dark-avatar.png' : '/image/firefly-light-avatar.png';
 }
 
@@ -27,14 +33,12 @@ export const Avatar = memo(function Avatar({ src, size, className, fallbackUrl, 
     const isDarkMode = useIsDarkMode();
     const [hasError, setHasError] = useState(false);
 
-    const isNormalUrl = !!src && !src.startsWith('data:image/') && !isDomainOrSubdomainOf(src, 'warpcast.com');
-    const { data: url } = useQuery({
-        enabled: isNormalUrl,
-        queryKey: ['avatar', isNormalUrl ? src : '[disabled-url]', fallbackUrl],
-        queryFn: () => resolveFirstAvailableUrl(compact([resolveImgurUrl(resolveAvatarFallbackUrl(src)), fallbackUrl])),
-    });
+    const url =
+        [resolveAvatarFallbackUrl, resolveImgurUrl].reduce((acc, fn) => (acc ? fn(acc, isDarkMode) : acc), src) ||
+        fallbackUrl;
 
     const defaultFallbackUrl = isDarkMode ? '/image/firefly-dark-avatar.png' : '/image/firefly-light-avatar.png';
+    const isNormalUrl = !!src && !src.startsWith('data:image/') && !isDomainOrSubdomainOf(src, 'warpcast.com');
     const imageSrc = hasError ? defaultFallbackUrl : (isNormalUrl ? url : src) || src || defaultFallbackUrl;
 
     return (
