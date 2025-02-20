b1f84b6652683d82c9466814aef88cf57f885db5
LeifXu <167842553+LeifXu@users.noreply.github.com>
2025-02-20 17:12:16 +1030

    feat: show all profile source tabs from api (#4115)

--- src/components/Profile/ProfileSourceTabs.tsx
+++ src/components/Profile/ProfileSourceTabs.tsx
@@ -6,24 +6,11 @@ import { SourceTabs } from '@/components/SourceTabs/index.js';
 import { SourceTab } from '@/components/SourceTabs/SourceTab.js';
 import type { ProfilePageSource } from '@/constants/enum.js';
 import { SORTED_PROFILE_SOURCES } from '@/constants/index.js';
-import { isSameFireflyIdentity } from '@/helpers/isSameFireflyIdentity.js';
 import { resolveProfileUrl } from '@/helpers/resolveProfileUrl.js';
 import { resolveSourceName } from '@/helpers/resolveSourceName.js';
-import { useCurrentFireflyProfilesAll } from '@/hooks/useCurrentFireflyProfiles.js';
 import type { FireflyIdentity, FireflyProfile } from '@/providers/types/Firefly.js';
 
-export function ProfileSourceTabs({
-    profiles: otherProfiles,
-    identity,
-}: {
-    profiles: FireflyProfile[];
-    identity: FireflyIdentity;
-}) {
-    const currentProfiles = useCurrentFireflyProfilesAll();
-    const isCurrentProfile = currentProfiles.some((x) => isSameFireflyIdentity(x.identity, identity));
-
-    const profiles = isCurrentProfile ? currentProfiles : otherProfiles;
-
+export function ProfileSourceTabs({ profiles, identity }: { profiles: FireflyProfile[]; identity: FireflyIdentity }) {
     const resolveUrl = (x: ProfilePageSource) => {
         const profile = profiles.find((profile) => profile.identity.source === x);
         return resolveProfileUrl(x, profile?.identity.id ?? identity.id);
