43607eeb6db39bb3ac8dd5949fc62a994e67587e
LeifXu <167842553+LeifXu@users.noreply.github.com>
2025-02-20 13:22:23 +1030

    fix: bsky firefly identity id empty string (#4107)

--- src/helpers/formatFireflyProfilesFromWalletProfiles.ts
+++ src/helpers/formatFireflyProfilesFromWalletProfiles.ts
@@ -5,58 +5,60 @@ import { formatEthereumAddress, formatSolanaAddress } from '@/helpers/formatAddr
 import type { WalletProfiles } from '@/providers/types/Firefly.js';
 
 export function formatFireflyProfilesFromWalletProfiles(profiles: WalletProfiles) {
-    return compact([
-        ...profiles.walletProfiles.map((x) => ({
-            identity: {
-                id: x.address,
-                source: Source.Wallet,
-            },
-            displayName: x.primary_ens || formatEthereumAddress(x.address, 4),
-            __origin__: x,
-        })),
-        ...profiles.solanaWalletProfiles.map((x) => ({
-            identity: {
-                id: x.address,
-                source: Source.Wallet,
-            },
-            displayName: x.primary_ens || formatSolanaAddress(x.address, 4),
-            __origin__: x,
-        })),
-        ...profiles.farcasterProfiles.map((x) => ({
-            identity: {
-                id: `${x.fid}`,
-                source: Source.Farcaster,
-            },
-            displayName: x.username,
-            __origin__: x,
-        })),
-        ...profiles.lensProfilesV3.map((x) => ({
-            identity: {
-                id: x.localName,
-                source: Source.Lens,
-            },
-            displayName: x.localName,
-            __origin__: x,
-        })),
-        ...profiles.twitterProfiles.map((x) => ({
-            identity: {
-                id: x.twitter_id,
-                source: Source.Twitter,
-            },
-            displayName: x.handle,
-            __origin__: x,
-        })),
-        ...profiles.bskyProfiles.map((x) => {
-            const prefix = 'bsky_';
-            const identityId = x.handle.startsWith(prefix) ? x.handle.substring(prefix.length) : x.handle;
-            return {
+    return compact(
+        [
+            ...profiles.walletProfiles.map((x) => ({
                 identity: {
-                    id: identityId,
-                    source: Source.Bsky,
+                    id: x.address,
+                    source: Source.Wallet,
+                },
+                displayName: x.primary_ens || formatEthereumAddress(x.address, 4),
+                __origin__: x,
+            })),
+            ...profiles.solanaWalletProfiles.map((x) => ({
+                identity: {
+                    id: x.address,
+                    source: Source.Wallet,
+                },
+                displayName: x.primary_ens || formatSolanaAddress(x.address, 4),
+                __origin__: x,
+            })),
+            ...profiles.farcasterProfiles.map((x) => ({
+                identity: {
+                    id: `${x.fid}`,
+                    source: Source.Farcaster,
+                },
+                displayName: x.username,
+                __origin__: x,
+            })),
+            ...profiles.lensProfilesV3.map((x) => ({
+                identity: {
+                    id: x.localName,
+                    source: Source.Lens,
+                },
+                displayName: x.localName,
+                __origin__: x,
+            })),
+            ...profiles.twitterProfiles.map((x) => ({
+                identity: {
+                    id: x.twitter_id,
+                    source: Source.Twitter,
                 },
                 displayName: x.handle,
                 __origin__: x,
-            };
-        }),
-    ]);
+            })),
+            ...profiles.bskyProfiles.map((x) => {
+                const prefix = 'bsky_';
+                const identityId = x.handle.startsWith(prefix) ? x.handle.substring(prefix.length) : x.handle;
+                return {
+                    identity: {
+                        id: identityId,
+                        source: Source.Bsky,
+                    },
+                    displayName: x.handle,
+                    __origin__: x,
+                };
+            }),
+        ].filter((x) => x.identity.id),
+    );
 }
