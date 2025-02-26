8ec2c92529b52f2e2437d080e42ff54e8aeafd6b
Wukong Sun <swkatmask@gmail.com>
2025-02-26 16:17:48 +0800

    refactor: consistent markdown plugin order and configuration (#4191)
    * refactor: consistent markdown plugin order
    
    * refactor: type safe
    
    * refactor: rename
    
    * chore: code maintenance [bot]
    
    ---------
    
    Co-authored-by: guanbinrui <guanbinrui@mask.io>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- src/components/Markup/Markup.tsx
+++ src/components/Markup/Markup.tsx
@@ -11,17 +11,12 @@ import stripMarkdown from 'strip-markdown';
 import { Code } from '@/components/Code.js';
 import { MarkupLink } from '@/components/Markup/MarkupLink/index.js';
 import { UrlPlugin } from '@/components/Markup/plugins/UrlPlugin.js';
-import {
-    CHANNEL_REGEX,
-    EMAIL_REGEX,
-    HASHTAG_REGEX,
-    LENS_HANDLE_REGEXP,
-    SYMBOL_REGEX,
-    URL_REGEX,
-} from '@/constants/regexp.js';
+import { CHANNEL_REGEX, EMAIL_REGEX, HASHTAG_REGEX, LENS_HANDLE_REGEXP, SYMBOL_REGEX } from '@/constants/regexp.js';
 import { isChannelSupported } from '@/helpers/isChannelSupported.js';
 import { trimify } from '@/helpers/trimify.js';
 import type { Post } from '@/providers/types/SocialMedia.js';
+
+type Pluggable = NonNullable<Parameters<typeof ReactMarkdown>[0]['remarkPlugins']>[number];
 
 export interface MarkupProps extends Omit<ReactMarkdownOptions, 'children'> {
     children?: ReactMarkdownOptions['children'] | null;
@@ -32,22 +27,13 @@ function Ol(props: DetailedHTMLProps<OlHTMLAttributes<HTMLOListElement>, HTMLOLi
     return <ol {...props} style={{ counterReset: `list-counter ${props.start ? props.start - 1 : ''}` }} />;
 }
 export const Markup = memo<MarkupProps>(function Markup({ children, post, ...rest }) {
-    const plugins = useMemo(() => {
-        if (!post?.mentions?.length)
-            return compact([
-                [stripMarkdown, { keep: ['strong', 'emphasis', 'inlineCode', 'list', 'listItem'] }],
-                remarkBreaks,
-                linkifyRegex(EMAIL_REGEX),
-                UrlPlugin,
-                // parsing handle after url
-                // for example https://images.lens.phaver.com/insecure/raw:t/plain/3daf21dbbf8ce530685bbfabf5de325d
-                linkifyRegex(LENS_HANDLE_REGEXP),
-                isChannelSupported(post?.source) ? linkifyRegex(CHANNEL_REGEX) : undefined,
-                linkifyRegex(HASHTAG_REGEX),
-                linkifyRegex(SYMBOL_REGEX),
-            ]);
-        const handles = post.mentions.map((x) => x.fullHandle);
-        const mentionRe = new RegExp(`@(${handles.join('|')})`, 'g');
+    const plugins: Pluggable[] = useMemo(() => {
+        let MentionPlugin: Pluggable | null = null;
+        if (post?.mentions?.length) {
+            const handles = post.mentions.map((x) => x.fullHandle);
+            const mentionRe = new RegExp(`@(${handles.join('|')})`, 'g');
+            MentionPlugin = linkifyRegex(mentionRe);
+        }
         return compact([
             [stripMarkdown, { keep: ['strong', 'emphasis', 'inlineCode', 'list', 'listItem'] }],
             remarkBreaks,
@@ -55,10 +41,12 @@ export const Markup = memo<MarkupProps>(function Markup({ children, post, ...res
             // Make sure Mention plugin is before URL plugin, to avoid matching
             // mentioned ens handle as url. For example, @mask.eth should be treat
             // as a mention rather than link
-            linkifyRegex(mentionRe),
-            linkifyRegex(URL_REGEX),
+            MentionPlugin,
+            UrlPlugin,
+            // parsing handle after url
+            // for example https://images.lens.phaver.com/insecure/raw:t/plain/3daf21dbbf8ce530685bbfabf5de325d
             linkifyRegex(LENS_HANDLE_REGEXP),
-            isChannelSupported(post.source) ? linkifyRegex(CHANNEL_REGEX) : undefined,
+            isChannelSupported(post?.source) ? linkifyRegex(CHANNEL_REGEX) : undefined,
             linkifyRegex(HASHTAG_REGEX),
             linkifyRegex(SYMBOL_REGEX),
         ]);
