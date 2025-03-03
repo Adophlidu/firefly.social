d644d29acc2ebf708b9d85e73d6bdbca1162f172
LeifXu <167842553+LeifXu@users.noreply.github.com>
2025-03-03 17:35:33 +1030

    fix: blink adapter connect (#4223)

--- src/hooks/useActionAdapter.ts
+++ src/hooks/useActionAdapter.ts
@@ -1,4 +1,4 @@
-import { BlockchainIds, createSignMessageText } from '@dialectlabs/blinks';
+import { type BlinkExecutionContext, BlockchainIds, createSignMessageText } from '@dialectlabs/blinks';
 import { useEvmWagmiAdapter } from '@dialectlabs/blinks/hooks/evm';
 import { BlinkSolanaConfig } from '@dialectlabs/blinks-core/solana';
 import { useAppKitConnection } from '@reown/appkit-adapter-solana/react';
@@ -9,8 +9,8 @@ import { useMemo } from 'react';
 import { chains } from '@/configs/wagmiClient.js';
 import { decodeBase64 } from '@/helpers/decodeBase64.js';
 import { getSolanaRPCUrl } from '@/helpers/getSolanaRPCUrl.js';
-import { useSolanaWalletProvider } from '@/hooks/useSolanaWalletProvider.js';
 import { ConnectModalRef } from '@/modals/controls.js';
+import { getWalletAdaptorConnected } from '@/providers/solana/getWalletAdapter.js';
 
 export function useActionAdapter() {
     const { adapter: evmAdapter } = useEvmWagmiAdapter({
@@ -19,27 +19,20 @@ export function useActionAdapter() {
         },
     });
     const { connection } = useAppKitConnection();
-    const walletProvider = useSolanaWalletProvider();
     const solanaAdapter = useMemo(() => {
         return new BlinkSolanaConfig(connection ?? getSolanaRPCUrl(), {
             async signMessage(data) {
-                if (!walletProvider) {
-                    await ConnectModalRef.openAndWaitForClose();
-                    return { error: 'Connection error' };
-                }
+                const adapter = getWalletAdaptorConnected();
                 const text = typeof data === 'string' ? data : createSignMessageText(data);
                 const encoded = new TextEncoder().encode(text);
-                const signed = await walletProvider?.signMessage(encoded);
+                const signed = await adapter.signMessage(encoded);
                 const encodedSignature = bs58.encode(signed);
                 return { signature: encodedSignature };
             },
             async signTransaction(txData) {
                 try {
-                    if (!walletProvider) {
-                        await ConnectModalRef.openAndWaitForClose();
-                        return { error: 'Connection error' };
-                    }
-                    const tx = await walletProvider?.signAndSendTransaction(
+                    const adapter = getWalletAdaptorConnected();
+                    const tx = await adapter.signAndSendTransaction(
                         VersionedTransaction.deserialize(decodeBase64(txData)),
                     );
                     return { signature: tx };
@@ -48,31 +41,33 @@ export function useActionAdapter() {
                 }
             },
             async connect() {
-                await ConnectModalRef.openAndWaitForClose();
-                return null;
+                try {
+                    const adapter = getWalletAdaptorConnected();
+                    return adapter?.publicKey?.toBase58() ?? null;
+                } catch {
+                    await ConnectModalRef.openAndWaitForClose();
+                    return null;
+                }
             },
         });
-    }, [connection, walletProvider]);
+    }, [connection]);
 
     return useMemo(() => {
+        const isEVM = (context: BlinkExecutionContext) =>
+            context.action.metadata.blockchainIds?.some((x) => x.startsWith('eip155:'));
+
         return new BlinkSolanaConfig(connection ?? getSolanaRPCUrl(), {
             async signMessage(data, context) {
-                const isEVM = context.action.metadata.blockchainIds?.some((x) => x.startsWith('eip155:'));
-                if (isEVM) {
-                    return evmAdapter.signMessage(data, context);
-                }
+                if (isEVM(context)) evmAdapter.signMessage(data, context);
                 return solanaAdapter.signMessage(data, context);
             },
             async signTransaction(txData, context) {
-                const isEVM = context.action.metadata.blockchainIds?.some((x) => x.startsWith('eip155:'));
-                if (isEVM) {
-                    return evmAdapter.signMessage(txData, context);
-                }
+                if (isEVM(context)) return evmAdapter.signMessage(txData, context);
                 return solanaAdapter.signTransaction(txData, context);
             },
-            async connect() {
-                await ConnectModalRef.openAndWaitForClose();
-                return null;
+            async connect(context) {
+                if (isEVM(context)) return evmAdapter.connect(context);
+                return solanaAdapter.connect(context);
             },
             metadata: {
                 supportedBlockchainIds: [
