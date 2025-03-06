b155c387a95cb5f985a4b1a7f8bad85bf42ce2f0
LeifXu <167842553+LeifXu@users.noreply.github.com>
2025-03-06 11:46:40 +1030

    fix: show condition discover filter (#4258)

--- src/components/HomeTab/index.tsx
+++ src/components/HomeTab/index.tsx
@@ -99,7 +99,7 @@ export function HomeTabs() {
                         }));
                     }}
                 />
-                {source !== Source.Posts ? <DiscoverFilter tab={currentTab} source={source} /> : null}
+                {source === Source.Posts ? <DiscoverFilter tab={currentTab} source={source} /> : null}
             </div>
         </div>
     );
