ba3742fc67c8736aea2cc5ade90d32110cb540b1
dudu0506 <dudu@mask.io>
2025-03-14 17:40:28 +0800

    fix: link plugin (#4372)

--- src/components/Markup/plugins/UrlPlugin.ts
+++ src/components/Markup/plugins/UrlPlugin.ts
@@ -1,28 +1,32 @@
+import { compact } from 'lodash-es';
 import type { PhrasingContent, Root, RootContent, Text } from 'mdast';
 import linkifyRegex from 'remark-linkify-regex';
 
 import { URL_REGEX } from '@/constants/regexp.js';
 
 function removeMentionFromLink<T extends PhrasingContent | RootContent>(nodes: T[]): Array<T | Text> {
-    return nodes.map((node, index) => {
-        const prevNode = nodes[index - 1];
-        if (
-            node.type === 'link' &&
-            !node.url.startsWith('http') &&
-            prevNode?.type === 'text' &&
-            prevNode.value.endsWith('@')
-        ) {
-            return {
-                type: 'text',
-                value: node.title!,
-            } satisfies Text;
-        }
-        if ('children' in node && node.children?.length) {
-            node.children = removeMentionFromLink(node.children as PhrasingContent[]);
-        }
+    return compact(
+        nodes.map((node, index) => {
+            const prevNode = nodes[index - 1];
+            if (
+                node.type === 'link' &&
+                !node.url.startsWith('http') &&
+                prevNode?.type === 'text' &&
+                (prevNode.value.endsWith('@') || /\s+@\S+$/.test(prevNode.value))
+            ) {
+                nodes[index - 1] = null as unknown as T;
+                return {
+                    type: 'text',
+                    value: `${prevNode.value}${node.title}`,
+                } satisfies Text;
+            }
+            if ('children' in node && node.children?.length) {
+                node.children = removeMentionFromLink(node.children as PhrasingContent[]);
+            }
 
-        return node;
-    });
+            return node;
+        }),
+    );
 }
 
 export function UrlPlugin() {
