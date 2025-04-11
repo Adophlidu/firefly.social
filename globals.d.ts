e93f1e7fd6edac92f3dde87c0af8a51ca6d8e530
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2025-04-11 21:16:08 +0800

    refactor: rm maskbook (#4637)

--- globals.d.ts
+++ globals.d.ts
@@ -1,9 +1,3 @@
-/// <reference types="./src/maskbook/packages/encryption/src/internal.d.ts" />
-/// <reference types="./src/maskbook/packages/polyfills/types/__all.d.ts" />
-/// <reference types="./src/maskbook/packages/polyfills/types/dom.d.ts" />
-/// <reference types="./src/maskbook/packages/polyfills/types/firefox.d.ts" />
-/// <reference types="./src/maskbook/packages/web3-telemetry/src/env.d.ts" />
-
 declare module 'dompurify' {
     class DOMPurity {
         sanitize: (html: string) => string;
--- lingui.config.js
+++ lingui.config.js
@@ -25,7 +25,6 @@ export default {
                 'src/services/**',
                 'src/mask/**',
             ],
-            exclude: ['src/maskbook/**'],
         },
     ],
     fallbackLocales: {
