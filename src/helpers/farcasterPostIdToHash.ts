61ae03c21a34ddd5f4bc833a9c998bb6604113e2
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2025-04-18 19:47:50 +0800

    Update src/helpers/farcasterPostIdToHash.ts

--- src/helpers/farcasterPostIdToHash.ts
+++ src/helpers/farcasterPostIdToHash.ts
@@ -1,4 +1,5 @@
 export function farcasterPostIdToHash(postId: string) {
+    console.log('DEBUG: farcasterPostIdToHash', postId);
     const hexWithoutPrefix = postId.startsWith('0x') ? postId.slice(2) : postId;
     const uint8Array = new Uint8Array(hexWithoutPrefix.length / 2);
 
