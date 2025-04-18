21c345adf6a0621dd6e744b5e49bc1809ab569ef
dudu0506 <dudu@mask.io>
2025-04-18 12:56:35 +0800

    fix: resolve cast embeds from tako (#4730)

--- src/providers/firefly/SocialMedia.ts
+++ src/providers/firefly/SocialMedia.ts
@@ -386,7 +386,7 @@ export class FireflySocialMedia implements Provider {
                 method: 'GET',
             });
 
-            const post = cast ? await formatFarcasterPostFromFirefly(cast) : null;
+            const post = cast ? await formatFarcasterPostFromFirefly(cast, undefined, true) : null;
             if (!post) throw new NotFoundError('Post not found');
             return post;
         });
