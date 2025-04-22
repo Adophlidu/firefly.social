84adb8bb90688c00b3bd48645696d595532389bb
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2025-04-22 11:50:03 +0800

    chore: up nitter root url (#4763)

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -48,7 +48,7 @@ const ExternalEnvSchema = z.object({
     NEXT_PUBLIC_FARCASTER_OPENRANK_URL: z.string().default('https://graph.cast.k3l.io'),
     NEXT_PUBLIC_LENS_OPENRANK_URL: z.string().default('https://lens-api.k3l.io'),
     NEXT_PUBLIC_SOLANA_RPC_URL: z.string().default('https://api.mainnet-beta.solana.com'),
-    NEXT_PUBLIC_FIREFLY_NITTER_URL: z.string().default('https://nitter-api.firefly.social'),
+    NEXT_PUBLIC_FIREFLY_NITTER_URL: z.string().default('https://nitter.r2d2.to'),
 
     // features
     NEXT_PUBLIC_POLL: z.nativeEnum(STATUS).default(STATUS.Disabled),
