fb168b7a26061388d739146f92b02188218e7f52
dudu0506 <dudu@mask.io>
2025-04-24 14:18:18 +0800

    fix: error boundary for rp card (#4795)

--- src/components/RedPacket/Timer.tsx
+++ src/components/RedPacket/Timer.tsx
@@ -2,13 +2,18 @@
 
 import { Trans } from '@lingui/react/macro';
 import dayjs from 'dayjs';
-import { useState } from 'react';
+import { type HTMLProps, useState } from 'react';
 import { useInterval } from 'usehooks-ts';
 
 import HourGlassIcon from '@/assets/hourglass.svg';
+import { classNames } from '@/helpers/classNames.js';
 import { getTimeLeft } from '@/helpers/formatTimestamp.js';
 
-export function Timer({ endTime }: { endTime: number }) {
+interface TimerProps extends HTMLProps<HTMLDivElement> {
+    endTime: number;
+}
+
+export function Timer({ endTime, className }: TimerProps) {
     const [now, setNow] = useState(Date.now());
 
     const timeLeft = getTimeLeft(endTime, now);
@@ -22,7 +27,12 @@ export function Timer({ endTime }: { endTime: number }) {
     );
     if (isExpired || !timeLeft) return null;
     return (
-        <div className="flex items-center justify-center gap-[6px] text-nowrap rounded-full bg-[#E8E8FF] px-[13px] py-[7px] opacity-75 backdrop-blur-[5px]">
+        <div
+            className={classNames(
+                'flex items-center justify-center gap-[6px] text-nowrap rounded-full bg-[#E8E8FF] px-[13px] py-[7px] opacity-75 backdrop-blur-[5px]',
+                className,
+            )}
+        >
             <HourGlassIcon width={12} height={12} />
             <span className="flex-1 text-xs leading-4">
                 <Trans>
