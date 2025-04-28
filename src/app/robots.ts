5e3a47b1c1fe09e620057630123ae8242c116aa4
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2025-04-28 12:28:07 +0800

    refactor: robots.txt (#4853)
    * chore: not allow for non-prod env
    
    * refactor: code style
    
    * chore: code maintenance [bot]
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,12 +1,28 @@
 import type { MetadataRoute } from 'next';
 
+import { IS_PRODUCTION } from '@/constants/index.js';
+
 export default function robots(): MetadataRoute.Robots {
+    // Block everything on staging, preview, testing, etc
+    if (!IS_PRODUCTION) {
+        return {
+            rules: [
+                {
+                    userAgent: '*',
+                    disallow: '/',
+                },
+            ],
+        };
+    }
+
+    // Allow crawling on production
     return {
-        rules: {
-            userAgent: '*',
-            allow: '/',
-            disallow: '',
-        },
+        rules: [
+            {
+                userAgent: '*',
+                allow: '/',
+            },
+        ],
         sitemap: 'https://firefly.social/sitemap.txt',
     };
 }
