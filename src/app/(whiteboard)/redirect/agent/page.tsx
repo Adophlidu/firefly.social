a6133b20f172cafb6f5e00fc140518eb4d3d4679
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2025-05-01 00:39:52 +0800

    fix: redirect error (#4908)
    * fix: redirect error
    
    * fix: redirect error

--- src/app/(whiteboard)/redirect/agent/page.tsx
+++ src/app/(whiteboard)/redirect/agent/page.tsx
@@ -12,9 +12,19 @@ import { createLookupTableResolver } from '@/helpers/createLookupTableResolver.j
 import { fireflyBridgeProvider } from '@/providers/firefly/Bridge.js';
 import { SupportedMethod } from '@/types/bridge.js';
 
-const getAgentType = () => {
-    if (fireflyBridgeProvider.supported) return Agent.FireflyApp;
-    return Agent.Browser;
+const action = async () => {
+    const agent = fireflyBridgeProvider.supported ? Agent.FireflyApp : Agent.Browser;
+
+    const formData = new FormData();
+    formData.append('agent', agent);
+
+    if (agent === Agent.FireflyApp) {
+        const language = await fireflyBridgeProvider.request(SupportedMethod.GET_LANGUAGE, {});
+        formData.append('locale', resolveLocale(language as 'en' | 'zh'));
+    }
+
+    await delay(1000);
+    await changeCookies(formData);
 };
 
 const resolveLocale = createLookupTableResolver<'en' | 'zh', Locale>(
@@ -27,25 +37,14 @@ const resolveLocale = createLookupTableResolver<'en' | 'zh', Locale>(
 
 export default function AgentPage() {
     const searchParams = useSearchParams();
-    const returnUrl = searchParams.get('url');
 
     useAsync(async () => {
-        const agent = getAgentType();
+        await action();
 
-        const formData = new FormData();
-        formData.append('agent', agent);
-
-        if (agent === Agent.FireflyApp) {
-            const language = await fireflyBridgeProvider.request(SupportedMethod.GET_LANGUAGE, {});
-            formData.append('locale', resolveLocale(language as 'en' | 'zh'));
-        }
-
-        await delay(1000);
-        await changeCookies(formData);
-        const url = returnUrl ?? PageRoute.Home;
+        const url = searchParams.get('url') ?? PageRoute.Home;
         const isValidPageRoute = url && getEnumAsArray(PageRoute).some(({ value }) => value === url);
         redirect(isValidPageRoute ? url : PageRoute.Home);
-    }, [returnUrl]);
+    }, []);
 
     return (
         <div className="fixed inset-0 z-10 flex h-screen items-center justify-center">
