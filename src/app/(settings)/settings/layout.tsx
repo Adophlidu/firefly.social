99c5b27a399ad5e05dfa7a3f278f5c1ef71972dd
Wukong Sun <swkatmask@gmail.com>
2025-05-13 16:53:59 +0800

    fixup! fix: settings layout (#4779) (#4987)

--- src/app/(settings)/settings/layout.tsx
+++ src/app/(settings)/settings/layout.tsx
@@ -8,7 +8,7 @@ export default async function Layout({ children }: { children: React.ReactNode }
         <>
             <main className="flex h-screen w-full flex-1 flex-col pl-0 md:pl-[235px] lg:flex-row lg:pl-[289px]">
                 {/* desktop */}
-                <div className="no-scrollbar hidden overflow-y-auto lg:flex">
+                <div className="no-scrollbar hidden shrink-0 overflow-y-auto lg:flex">
                     <SettingsList />
                 </div>
                 <div className="no-scrollbar w-full min-w-0 overflow-y-auto">{children}</div>
