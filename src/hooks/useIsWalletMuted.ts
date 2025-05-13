fc0b9978f5c4670439f40c9efde1271bb95c76fc
LeifXu <167842553+LeifXu@users.noreply.github.com>
2025-05-13 14:03:47 +0930

    fix: solana mute status (#4979)

--- src/hooks/useIsWalletMuted.ts
+++ src/hooks/useIsWalletMuted.ts
@@ -1,10 +1,11 @@
 import { useQuery } from '@tanstack/react-query';
 
 import { FireflyPlatform } from '@/constants/enum.js';
+import { isValidAddressEthereum } from '@/helpers/isValidAddress.js';
 import { FireflyEndpointProvider } from '@/providers/firefly/Endpoint.js';
 
 export function useIsWalletMuted(address: string, enabled = true) {
-    const addr = address.toLowerCase();
+    const addr = isValidAddressEthereum(address) ? address.toLowerCase() : address;
     return useQuery({
         enabled,
         queryKey: ['address-is-muted', addr],
