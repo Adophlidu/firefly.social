ab9f57c476078fa86122e9d537b952ac2a48cc91
dudu0506 <dudu@mask.io>
2025-05-15 09:44:21 +0800

    fix: remove domain migration alert (#5018)
    * fix: remove domain migration alert
    
    * chore: code maintenance [bot]
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- src/app/layout-body.tsx
+++ src/app/layout-body.tsx
@@ -1,7 +1,5 @@
-import { DomainMigrationNotification } from '@/components/DomainMigrationNotification/index.js';
 import { IfHostname } from '@/components/IfHostname.js';
 import { IfPathname } from '@/components/IfPathname.js';
-import { NoSSR } from '@/components/NoSSR.js';
 import { Providers } from '@/components/Providers.js';
 import { RouteProgressBar } from '@/components/RouteProgressBar.js';
 import { SideBar } from '@/components/SideBar/index.js';
@@ -61,9 +59,6 @@ export function LayoutBody({ children }: { children: React.ReactNode }) {
                     </div>
 
                     <Modals />
-                    <NoSSR>
-                        <DomainMigrationNotification />
-                    </NoSSR>
                     <Script>{REMOVE_LOADING_SCRIPT}</Script>
                 </RouteProgressBar>
             </Providers>
