dc47866ef169d9adf524e95f472d93090645729b
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2025-05-24 09:16:35 +0800

    chore: delay for replacing url (#5149)
    * chore: delay for replacing url
    
    * chore: code maintenance [bot]
    
    * fix: redirect error
    
    * fix: logs
    
    * fix: redirect
    
    * fix: redirect
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- src/app/(whiteboard)/redirect/agent/page.tsx
+++ src/app/(whiteboard)/redirect/agent/page.tsx
@@ -1,37 +1,37 @@
 'use client';
 
+import { getEnumAsArray } from '@masknet/kit';
+import { redirect } from 'next/navigation.js';
 import { useAsync } from 'react-use';
-import urlcat from 'urlcat';
 
+import { changeCookies } from '@/actions/changeCookies.js';
 import FireflyIcon from '@/assets/logo.svg';
 import { Agent, PageRoute } from '@/constants/enum.js';
-import { useRouter, useSearchParams } from '@/esm/navigation.js';
-import { fetchJSON } from '@/helpers/fetchJSON.js';
-import { isValidEnumValue } from '@/helpers/isValidEnumValue.js';
-import { runInSafeAsync } from '@/helpers/runInSafe.js';
+import { useSearchParams } from '@/esm/navigation.js';
 import { fireflyBridgeProvider } from '@/providers/firefly/Bridge.js';
 
 const action = async () => {
     const agent = fireflyBridgeProvider.supported ? Agent.FireflyApp : Agent.Browser;
-    await fetchJSON(
-        urlcat('/api/settings/agent', {
-            agent,
-        }),
-        {
-            method: 'POST',
-        },
-    );
+    const formData = new FormData();
+    formData.append('agent', agent);
+    await changeCookies(formData);
 };
 
 export default function AgentPage() {
-    const router = useRouter();
     const searchParams = useSearchParams();
 
     useAsync(async () => {
-        await runInSafeAsync(async () => action());
+        await action();
 
         const url = searchParams.get('url') ?? PageRoute.Home;
-        router.replace(isValidEnumValue(url, PageRoute) ? url : PageRoute.Home);
+        const isValidPageRoute = url && getEnumAsArray(PageRoute).some(({ value }) => value === url);
+
+        try {
+            redirect(isValidPageRoute ? url : PageRoute.Home);
+        } catch (error) {
+            console.error('Redirect error:', error);
+            // Ignore the error and continue
+        }
 
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
