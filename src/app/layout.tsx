7771ada2ee7cd69b82093d3a1f84008c54cedd2e
LeifXu <167842553+LeifXu@users.noreply.github.com>
2025-05-28 14:18:00 +0930

    feat: add cookie3 analytics (#5190)
    * feat: add cookie3 analytics
    
    * feat: add cookie3 analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,6 +41,14 @@ export default async function RootLayout({ children }: { children: React.ReactNo
             <head>
                 <Script src="/js/polyfills/base.js" strategy="beforeInteractive" />
                 <Script src="/js/safary.js" defer />
+                <Script
+                    src="/js/cookie3.analytics.js"
+                    integrity="sha384-ihnQ09PGDbDPthGB3QoQ2Heg2RwQIDyWkHkqxMzq91RPeP8OmydAZbQLgAakAOfI"
+                    crossOrigin="anonymous"
+                    async
+                    strategy="lazyOnload"
+                    site-id="4e0dc4ab-2a63-4303-ad25-8aa14275d2d4"
+                />
                 <GoogleAnalytics gaId="G-61NFDTK6LT" />
                 <meta name="theme-color" content="#ffffff" />
                 <meta name="googlebot" content="notranslate" />
