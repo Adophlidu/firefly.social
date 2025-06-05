6dfe904c17977c27c83ca837ac994251bb88994f
LeifXu <167842553+LeifXu@users.noreply.github.com>
2025-06-05 12:07:44 +0930

    feat: developers post link validator (#5247)
    * feat: developers post link validator
    
    * chore: code maintenance [bot]
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ src/app/(developers)/developers/postLink/layout.tsx
@@ -0,0 +1,14 @@
+import { msg } from '@lingui/core/macro';
+
+import { createPageTitleSSR } from '@/helpers/createPageTitle.js';
+import { createSiteMetadata } from '@/helpers/createSiteMetadata.js';
+
+export async function generateMetadata() {
+    return createSiteMetadata({
+        title: await createPageTitleSSR(msg`Post Link Validator`),
+    });
+}
+
+export default async function DetailLayout({ children }: { children: React.ReactNode }) {
+    return <>{children}</>;
+}
