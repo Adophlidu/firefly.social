26c420d3653c554aa9eb7d46a56784cf9a64eb4a
nuanyang233 <nuanyang233@gmail.com>
2025-06-20 12:49:11 +0800

    fix: bugfix for explore redirect (#5433)

--- src/helpers/parseOldExploreUrl.ts
+++ src/helpers/parseOldExploreUrl.ts
@@ -2,7 +2,7 @@ import { type ExploreSource, ExploreType, Source, TrendingType } from '@/constan
 
 export function parseOldExploreUrl(url: URL): { source: ExploreSource; type: ExploreType } | null {
     if (!url.pathname.startsWith('/explore')) return null;
-    const [, exploreTypeInUrl] = url.pathname.split('/');
+    const [, , exploreTypeInUrl] = url.pathname.split('/');
     if (exploreTypeInUrl === 'top-profiles') return { source: Source.Twitter, type: ExploreType.TopProfiles };
     if (exploreTypeInUrl === 'crypto-trends') {
         return { source: TrendingType.Trending, type: ExploreType.CryptoTrends };
