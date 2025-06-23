e21f99ff145c3ccdc222ead7d195f62160bb89f1
dudu0506 <dudu@mask.io>
2025-06-23 10:19:21 +0800

    fix: account asyncing status (#5440)

--- src/components/AccountConnectButton.tsx
+++ src/components/AccountConnectButton.tsx
@@ -5,15 +5,14 @@ import DoubleUser from '@/assets/double-user.svg';
 import { ClickableButton } from '@/components/ClickableButton.js';
 import { ProfileSourceIcon } from '@/components/ProfileSourceIcon.js';
 import { classNames } from '@/helpers/classNames.js';
-import { useAsyncStatusAll } from '@/hooks/useAsyncStatus.js';
-import { useCurrentProfiles } from '@/hooks/useCurrentProfile.js';
+import { useAsyncStatusAll, useSyncingSources } from '@/hooks/useAsyncStatus.js';
 
 interface Props {
     onClick: () => void;
 }
 
 export const AccountConnectButton = memo<Props>(function AccountConnectButton({ onClick }) {
-    const profiles = useCurrentProfiles();
+    const syncingSources = useSyncingSources();
     const isLoading = useAsyncStatusAll();
 
     return (
@@ -24,7 +23,7 @@ export const AccountConnectButton = memo<Props>(function AccountConnectButton({ 
                 'flex h-10 min-w-[120px] items-center gap-3 whitespace-nowrap rounded-lg bg-lightBg px-4 text-lg leading-6 text-main',
             )}
         >
-            {!profiles.length ? (
+            {!syncingSources.length ? (
                 <>
                     <DoubleUser />
                     <Trans>My Accounts</Trans>
@@ -33,11 +32,11 @@ export const AccountConnectButton = memo<Props>(function AccountConnectButton({ 
                 <>
                     <Trans>My Accounts</Trans>
                     <div className="flex items-center">
-                        {profiles.map((profile, index, self) => {
+                        {syncingSources.map((source, index, self) => {
                             return (
                                 <ProfileSourceIcon
                                     key={index}
-                                    source={profile.source}
+                                    source={source}
                                     className={index > 0 && self.length > 1 ? '-ml-2' : undefined}
                                     style={{ zIndex: self.length - index }}
                                 />
