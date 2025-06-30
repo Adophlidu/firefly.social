c29709030ec9da2fed6bc6e8257c5b63d441fad7
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2025-06-30 15:07:20 +0800

    fix: switch chain frame (#5505)

--- src/helpers/switchEthereumChain.ts
+++ src/helpers/switchEthereumChain.ts
@@ -6,6 +6,8 @@ import type { EthereumChainId } from '#masknet/web3-shared-evm';
 export async function switchEthereumChain(chainId: EthereumChainId) {
     const chain = chains.find((chain) => chain.id === chainId);
 
+    console.log('[switchEthereumChain] Switching to chain:', chainId, chain);
+
     await switchChain(config, {
         chainId,
         addEthereumChainParameter: chain
