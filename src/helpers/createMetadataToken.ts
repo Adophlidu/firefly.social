29dd4abd0b51756a83ba345f90fa2b81e9f891d1
dudu0506 <dudu@mask.io>
2025-07-14 12:32:33 +0800

    fix: remove duplicate code (#5618)

--- src/helpers/createMetadataToken.ts
+++ src/helpers/createMetadataToken.ts
@@ -29,7 +29,6 @@ export async function createMetadataToken(
     });
     if (!token) return createSiteMetadata(pathname);
 
-    if (!token) return createSiteMetadata(pathname);
     const title = createPageTitleOG(`$${token.symbol.toUpperCase()}`);
     const description = token.name;
     const ogImage = token.logoURL;
