92a9290882a70879268d15ead018b24e4a952813
dudu0506 <dudu@mask.io>
2025-07-21 17:56:44 +0800

    refactor: optimize global font (#5699)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,9 +41,17 @@ export default async function RootLayout({ children }: { children: ReactNode }) 
     ];
 
     return (
-        <html className={`font-loading ${rootClass}`}>
+        <html className={`${rootClass} ${inter.variable} font-inter`}>
             <head>
                 <Script src="/js/polyfills/base.js" strategy="beforeInteractive" />
+                {/* fonts */}
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
+                {/* eslint-disable-next-line @next/next/no-page-custom-font */}
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap"
+                    rel="stylesheet"
+                />
                 {IS_PRODUCTION ? <Script src="/js/safary.js" defer /> : null}
                 <Script>{VERCEL_REGION.join('\n')}</Script>
                 <Script
@@ -72,7 +80,7 @@ export default async function RootLayout({ children }: { children: ReactNode }) 
                 <link rel="preload" href="/webm/global-loading.webm" as="video" type="video/webm" />
                 {IS_PRODUCTION ? null : <meta name="robots" content="noindex, nofollow" />}
             </head>
-            <body className={`${inter.variable} notranslate font-inter`}>
+            <body className="notranslate">
                 <ErrorBoundary>
                     <LayoutBody>{children}</LayoutBody>
                 </ErrorBoundary>
