a6ea5cf9925e30ed3efd3823f225c08412a76137
guanbinrui <guanbinrui@mask.io>
2025-07-22 23:33:45 +0800

    chore: urlcat

--- src/providers/og/Processor.ts
+++ src/providers/og/Processor.ts
@@ -23,7 +23,7 @@ class Processor {
 
         const response = await fetchJSON<ResponseJSON<{ og: OpenGraph }>>(
             urlcat(FIREFLY_WORKER_HOST, '/oembed', {
-                link: documentUrl,
+                link: url.toString(),
             }),
             {
                 signal,
