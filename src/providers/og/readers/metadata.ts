ebf8dae4113543afc8fcb238580c4c8d71abffa7
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2025-08-01 17:41:48 +0800

    refactor: rm unused code (#5836)

--- src/providers/og/readers/metadata.ts
+++ src/providers/og/readers/metadata.ts
@@ -1,6 +1,5 @@
 /* cspell:disable */
 
-import { getMetaContent } from '@/helpers/getMetaContent.js';
 import { qAny } from '@/helpers/q.js';
 
 export function getTitle(document: Document): string | null {
@@ -13,32 +12,7 @@ export function getDescription(document: Document): string | null {
     return meta?.getAttribute('content') || null;
 }
 
-export function getSite(document: Document): string | null {
-    const meta = qAny(document, ['lens:site', 'og:site_name', 'twitter:site']);
-    return meta?.getAttribute('content') || null;
-}
-
 export function getImageUrl(document: Document): string | null {
     const meta = qAny(document, ['lens:image', 'og:image', 'twitter:image', 'twitter:image:src']);
     return meta?.getAttribute('content') || null;
-}
-
-export function getEmbedUrl(document: Document): string | null {
-    const meta = qAny(document, ['lens:player', 'og:video:url', 'og:video:secure_url', 'twitter:player']);
-    return meta?.getAttribute('content') || null;
-}
-
-export function getIsLarge(document: Document): boolean {
-    const lens = getMetaContent(document, 'lens:card');
-    const twitter = getMetaContent(document, 'twitter:card');
-
-    const largeTypes = ['summary_large_image', 'player'];
-
-    if (lens) {
-        return largeTypes.includes(lens);
-    } else if (twitter) {
-        return largeTypes.includes(twitter);
-    } else {
-        return false;
-    }
 }
