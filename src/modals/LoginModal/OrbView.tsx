46657b0b84145501f95c126ed3068be3b7efd770
nuanyang233 <nuanyang233@gmail.com>
2025-08-03 13:05:43 +0800

    chore: orb description i18n (#5846)

--- src/modals/LoginModal/OrbView.tsx
+++ src/modals/LoginModal/OrbView.tsx
@@ -151,7 +151,7 @@ export function OrbView() {
                     <div className="text-center text-xs leading-4 text-second">
                         <Trans>
                             Scan the QR code with the <span className="font-bold">Camera app</span> <br />
-                            and give<span className="mx-1 font-bold">edit permission</span>to sign in instantly
+                            and give <span className="font-bold">edit permission</span> to sign in instantly
                         </Trans>
                     </div>
                     <div
