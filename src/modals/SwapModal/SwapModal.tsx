cc2f2d83f3fe6c14980a0e917a006c6ecb44b4a7
Wukong Sun <swkatmask@gmail.com>
2025-08-05 21:14:30 +0800

    fix: okx widget theme now gets fixed (#5881)

--- src/modals/SwapModal/SwapModal.tsx
+++ src/modals/SwapModal/SwapModal.tsx
@@ -30,9 +30,8 @@ export function SwapModal({ ref }: Props) {
     });
 
     return (
-        // Fixed to dark mode since the OKX widget fails to response to config changes
-        <Modal open={open} onClose={() => dispatch?.close()} dialogClassName="dark">
-            <div className="relative z-10 w-[calc(100%-40px)] max-w-[400px] overflow-hidden rounded-2xl bg-white dark:bg-black">
+        <Modal open={open} onClose={() => dispatch?.close()}>
+            <div className="relative z-10 w-[calc(100vw-40px)] max-w-[400px] overflow-hidden rounded-2xl bg-white dark:bg-black">
                 {mounted ? <SwapModalContent props={props} open={open} onClose={() => dispatch?.close()} /> : null}
             </div>
         </Modal>
