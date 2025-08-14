f1db06ae4648b117c9c043fc7c30a4d281777002
LeifXu <167842553+LeifXu@users.noreply.github.com>
2025-08-14 16:57:30 +0930

    feat: add helper to retrieve Privy wallet client (#6007)

---
+++ src/helpers/getPrivyWalletClientRequired.ts
@@ -0,0 +1,36 @@
+import type { Config } from '@wagmi/core';
+import dayjs from 'dayjs';
+import { getConnectors, getWalletClient } from 'wagmi/actions';
+
+import { PRIVY_CONNECTOR_ID } from '@/connectors/PrivyConnector.js';
+import { LoginModalRef } from '@/modals/LoginModal/index.js';
+import { usePrivyWalletStore } from '@/store/usePrivyWalletsStore.js';
+import { useFireflyProfileStore } from '@/store/useProfileStore/useFireflyProfileStore.js';
+
+async function waitUntilReady(options?: { timeoutMs?: number; intervalMs?: number }) {
+    const timeoutMs = options?.timeoutMs ?? 10_000;
+    const intervalMs = options?.intervalMs ?? 1_000;
+    const start = Date.now();
+    if (usePrivyWalletStore.getState().ready) return;
+
+    while (!usePrivyWalletStore.getState().ready) {
+        if (Date.now() - start > timeoutMs) {
+            throw new Error('Timeout waiting for Privy to become ready');
+        }
+        await dayjs(intervalMs);
+    }
+}
+
+export async function getPrivyWalletClientRequired(config: Config) {
+    const isLogin = !!useFireflyProfileStore.getState().currentProfileSession;
+    if (!isLogin) {
+        await LoginModalRef.openAndWaitForClose();
+        await waitUntilReady();
+    }
+    const connectors = getConnectors(config);
+    const connector = connectors.find((connector) => connector.id === PRIVY_CONNECTOR_ID);
+
+    return await getWalletClient(config, {
+        connector,
+    });
+}
