be5408772e0b105f5cccee63623eb89cd6c4cfc1
dudu0506 <dudu@mask.io>
2025-08-20 19:33:26 +0800

    fix: avoid repeated call on orb api (#6048)

--- src/hooks/useRetrievePollFromPost.ts
+++ src/hooks/useRetrievePollFromPost.ts
@@ -1,6 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 
 import { Source } from '@/constants/enum.js';
+import { patchPostQueryData } from '@/helpers/patchPostQueryData.js';
 import { useCurrentProfile } from '@/hooks/useCurrentProfile.js';
 import { OrbProvider } from '@/providers/orb/index.js';
 import type { Post } from '@/providers/types/SocialMedia.js';
@@ -15,7 +16,16 @@ export function useRetrievePollFromPost(post: Post) {
         queryKey: ['post', 'poll', Source.Lens, post.postId, lensProfile?.profileId],
         enabled: queryLensPoll,
         staleTime: 1000 * 60 * 2, // 2 minutes
-        queryFn: () => OrbProvider.getPoll(post.postId, lensProfile?.profileId),
+        queryFn: async () => {
+            const poll = await OrbProvider.getPoll(post.postId, lensProfile?.profileId);
+            if (poll) {
+                patchPostQueryData(Source.Lens, post.postId, (draft) => {
+                    draft.poll = poll;
+                });
+            }
+
+            return poll;
+        },
     });
 
     return { poll: queryLensPoll ? lensPoll : poll, isLoading };
