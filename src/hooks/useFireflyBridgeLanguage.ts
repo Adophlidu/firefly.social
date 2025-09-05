9009b61b471938ce27ac310c5586ea1369f9c2a1
LeifXu <ins@mask.io>
2025-09-05 16:55:52 +0930

    fix: event language

--- src/hooks/useFireflyBridgeLanguage.ts
+++ src/hooks/useFireflyBridgeLanguage.ts
@@ -1,29 +1,31 @@
 'use client';
 
-import { useLingui } from '@lingui/react';
 import { useQuery } from '@tanstack/react-query';
 
+import { changeCookies } from '@/actions/changeCookies.js';
 import { Locale } from '@/constants/enum.js';
 import { fireflyBridgeProvider } from '@/providers/firefly/Bridge.js';
 import { SupportedMethod } from '@/types/bridge.js';
 
 export function useFireflyBridgeLanguage() {
-    const { i18n } = useLingui();
     return useQuery({
         enabled: fireflyBridgeProvider.supported,
         queryKey: ['firefly-bridge-language'],
         async queryFn() {
             const language = await fireflyBridgeProvider.request(SupportedMethod.GET_LANGUAGE, {});
+            const data = new FormData();
             switch (language) {
                 case 'zh':
-                    i18n.activate(Locale.zhHans);
+                    data.append('locale', Locale.zhHans);
                     break;
                 default:
-                    i18n.activate(language);
+                    data.append('locale', Locale.en);
                     break;
             }
+            await changeCookies(data);
         },
         refetchOnWindowFocus: false,
         refetchOnMount: false,
+        retry: 1,
     });
 }
