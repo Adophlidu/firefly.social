d305a90be5be1918549fa74e726561c25b49db99
Wukong Sun <swkatmask@gmail.com>
2025-09-12 16:41:11 +0800

    fix(miniapp): fw-5709 display author info in header (#6230)
    * fix(miniapp): fw-5709 display author info in header
    
    * fixup! fix(miniapp): fw-5709 display author info in header

--- src/modals/FrameViewerModal/FrameViewerModalHeader.tsx
+++ src/modals/FrameViewerModal/FrameViewerModalHeader.tsx
@@ -1,11 +1,16 @@
-import { useCallback } from 'react';
+import { Trans } from '@lingui/react/macro';
+import { useQuery } from '@tanstack/react-query';
+import { useCallback, useMemo } from 'react';
 import { useAsyncFn } from 'react-use';
 
 import { CloseButton } from '@/components/IconButton.js';
 import { Image } from '@/components/Image.js';
-import { NetworkType } from '@/constants/enum.js';
+import { ProfileVerifyBadge } from '@/components/ProfileVerifyBadge/index.js';
+import { NetworkType, Source } from '@/constants/enum.js';
 import { delay } from '@/helpers/delay.js';
+import { parseJson } from '@/helpers/parseJson.js';
 import { parseUrl } from '@/helpers/parseUrl.js';
+import { resolveSocialMediaProvider } from '@/helpers/resolveSocialMediaProvider.js';
 import type { FrameViewerModalOpenProps } from '@/modals/FrameViewerModal/FrameViewerModalContent.js';
 import { MoreAction } from '@/modals/FrameViewerModal/MoreActionMenu.js';
 import { WalletConnectModalRef } from '@/modals/WalletConnectModal/index.js';
@@ -35,6 +40,21 @@ export function FrameViewerModalHeader({ props, setProps, onClose }: FrameViewer
     const { frame } = props;
     const u = parseUrl(frame.button.action.url);
 
+    const authorFid = useMemo(() => {
+        if (!frame.x_manifest?.accountAssociation?.header) return null;
+        const raw = atob(frame.x_manifest.accountAssociation.header);
+        return parseJson<{ fid: number }>(raw)?.fid.toString();
+    }, [frame]);
+    const { data: author } = useQuery({
+        enabled: !!authorFid,
+        queryKey: ['profile', Source.Farcaster, authorFid],
+        queryFn: () => {
+            if (!authorFid) return null;
+            const provider = resolveSocialMediaProvider(Source.Farcaster);
+            return provider.getProfileById(authorFid, true);
+        },
+    });
+
     return (
         <div className="flex h-[60px] items-center justify-between bg-lightBg px-4 py-3 text-black dark:bg-fireflyBrand dark:text-white">
             <div className="cursor-pointer">
@@ -52,7 +72,17 @@ export function FrameViewerModalHeader({ props, setProps, onClose }: FrameViewer
                 ) : null}
                 <div className={frame.x_manifest?.frame.iconUrl ? 'text-left' : ''}>
                     <h2 className="font-bold">{frame.x_manifest?.frame.name || frame.button.action.name}</h2>
-                    {u ? <div className="text-xs text-secondary">{u.host}</div> : null}
+                    {author ? (
+                        <div className="flex gap-1 text-xs text-secondary">
+                            <Trans>by {author.fullHandle || author.displayName}</Trans>
+                            <ProfileVerifyBadge
+                                className="flex flex-shrink-0 items-center space-x-1"
+                                profile={author}
+                            />
+                        </div>
+                    ) : u ? (
+                        <div className="text-xs text-secondary">{u.host}</div>
+                    ) : null}
                 </div>
             </div>
             <div>
