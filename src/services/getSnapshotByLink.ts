2ea70e13909788fe62d9b88d217eaf5a864ad62b
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2025-09-29 14:08:44 +0800

    refactor: getSnapshotByLink (#6318)

--- src/services/getSnapshotByLink.ts
+++ src/services/getSnapshotByLink.ts
@@ -1,24 +1,16 @@
-import { SNAPSHOT_GRAPHQL_URL } from '@/constants/index.js';
+import urlcat from 'urlcat';
+import { FIREFLY_WORKER_HOST } from '@/constants/index.js';
 import { SNAPSHOT_NEW_PROPOSAL_REGEXP, SNAPSHOT_PROPOSAL_REGEXP } from '@/constants/regexp.js';
 import { fetchJson } from '@/helpers/fetchJson.js';
-import { ProposalQuery } from '@/providers/snapshot/query.js';
 import type { SnapshotProposal } from '@/providers/snapshot/type.js';
 
 export async function getSnapshotByLink(link: string) {
     if (!SNAPSHOT_PROPOSAL_REGEXP.test(link) && !SNAPSHOT_NEW_PROPOSAL_REGEXP.test(link)) return;
-    const match = link.match(SNAPSHOT_PROPOSAL_REGEXP);
-    const newMatch = link.match(SNAPSHOT_NEW_PROPOSAL_REGEXP);
-    const id = match ? match[1] : newMatch ? newMatch[2] : null;
-    if (!id) return;
 
-    const { data } = await fetchJson<{ data: { proposal: SnapshotProposal } }>(SNAPSHOT_GRAPHQL_URL, {
-        method: 'POST',
-        body: JSON.stringify({
-            ...ProposalQuery,
-            variables: {
-                id,
-            },
+    const { data } = await fetchJson<{ data: { proposal: SnapshotProposal } }>(
+        urlcat(FIREFLY_WORKER_HOST, '/snapshot', {
+            url: link,
         }),
-    });
+    );
     return data.proposal;
 }
