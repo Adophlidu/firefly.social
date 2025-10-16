e5c76e40c310b4cbff443f5c76465bc6e9d7fabc
Wukong Sun <swkatmask@gmail.com>
2025-10-16 11:02:22 +0800

    fix(bookmarks): extract BookmarkSourceTabs as a client component (#6400)

---
+++ src/app/(normal)/bookmarks/[source]/BookmarkSourceTabs.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import { SolidSourceTabs } from '@/components/Tabs/SolidSourceTabs.js';
+import { type BookmarkSource, Source } from '@/constants/enum.js';
+import { BOOKMARK_SOURCES } from '@/constants/index.js';
+import { resolveBookmarkUrl } from '@/helpers/resolveBookmarkUrl.js';
+import { captureBookmarkTokenViewEvent } from '@/providers/telemetry/captureTokenEvent.js';
+
+interface Props {
+    source: BookmarkSource;
+}
+
+export function BookmarkSourceTabs({ source }: Props) {
+    return (
+        <SolidSourceTabs
+            active={source}
+            sources={BOOKMARK_SOURCES.map((s) => ({ source: s, link: resolveBookmarkUrl(s) }))}
+            onChange={(source) => {
+                if (source === Source.Tokens) captureBookmarkTokenViewEvent('direct');
+            }}
+        />
+    );
+}
--- src/app/(normal)/bookmarks/[source]/layout.tsx
+++ src/app/(normal)/bookmarks/[source]/layout.tsx
@@ -1,20 +1,16 @@
 import { msg } from '@lingui/core/macro';
 import { Trans } from '@lingui/react/macro';
 
-import { NoSSR } from '@/components/NoSSR.js';
-import { SolidSourceTabs } from '@/components/Tabs/SolidSourceTabs.js';
+import { BookmarkSourceTabs } from '@/app/(normal)/bookmarks/[source]/BookmarkSourceTabs.js';
 import { TimelineTitle } from '@/components/TimelineTitle.js';
 import { Source } from '@/constants/enum.js';
-import { BOOKMARK_SOURCES } from '@/constants/index.js';
 import { notFound } from '@/esm/navigation/server.js';
 import { classNames } from '@/helpers/classNames.js';
 import { createPageTitleSSR } from '@/helpers/createPageTitle.js';
 import { createSiteMetadata } from '@/helpers/createSiteMetadata.js';
 import { isBookmarkSource } from '@/helpers/isSource.js';
-import { resolveBookmarkUrl } from '@/helpers/resolveBookmarkUrl.js';
 import { resolveSourceFromUrlNoFallback } from '@/helpers/resolveSource.js';
 import { setupLocaleForSSR } from '@/i18n/index.js';
-import { captureBookmarkTokenViewEvent } from '@/providers/telemetry/captureTokenEvent.js';
 import type { NextPageProps } from '@/types/utility.js';
 
 interface Props extends NextPageProps<{ source: string }> {}
@@ -41,15 +37,7 @@ export default async function Layout(props: Props) {
             <div className="sticky top-[54px] z-20 bg-primaryBottom pb-3 md:top-0">
                 <TimelineTitle title={<Trans>Bookmarks</Trans>} />
                 <div className="px-4">
-                    <NoSSR>
-                        <SolidSourceTabs
-                            active={source}
-                            sources={BOOKMARK_SOURCES.map((s) => ({ source: s, link: resolveBookmarkUrl(s) }))}
-                            onChange={(source) => {
-                                if (source === Source.Tokens) captureBookmarkTokenViewEvent('direct');
-                            }}
-                        />
-                    </NoSSR>
+                    <BookmarkSourceTabs source={source} />
                 </div>
             </div>
             <div className={classNames(source !== Source.Tokens ? 'px-4' : null)}>{children}</div>
