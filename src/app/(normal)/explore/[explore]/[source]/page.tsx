5d4fc68ac6e2338c5de6269b3bec6b8c3a4e8534
dudu0506 <dudu@mask.io>
2025-10-17 16:53:04 +0800

    fix: check source for explore page (#6418)

--- src/app/(normal)/explore/[explore]/[source]/page.tsx
+++ src/app/(normal)/explore/[explore]/[source]/page.tsx
@@ -4,6 +4,8 @@ import { use } from 'react';
 
 import { ExplorePage } from '@/app/(normal)/explore/pages/Explore.js';
 import { type ExploreSource, type ExploreSourceInURL, ExploreType, type TrendingType } from '@/constants/enum.js';
+import { EXPLORE_SOURCES } from '@/constants/index.js';
+import { notFound } from '@/esm/navigation.js';
 import { resolveSourceFromUrl } from '@/helpers/resolveSource.js';
 import type { NextPageProps } from '@/types/utility.js';
 
@@ -12,14 +14,14 @@ interface Props extends NextPageProps<{ source: ExploreSourceInURL; explore: Exp
 export default function Page(props: Props) {
     const { source, explore } = use(props.params);
 
-    return (
-        <ExplorePage
-            source={
-                explore === ExploreType.CryptoTrends
-                    ? (source as TrendingType)
-                    : (resolveSourceFromUrl(source) as ExploreSource)
-            }
-            type={explore}
-        />
-    );
+    const validSources = EXPLORE_SOURCES[explore];
+    const exploreSource =
+        explore === ExploreType.CryptoTrends
+            ? (source as TrendingType)
+            : (resolveSourceFromUrl(source) as ExploreSource);
+    if (validSources?.length && !validSources.includes(exploreSource)) {
+        notFound();
+    }
+
+    return <ExplorePage source={exploreSource} type={explore} />;
 }
