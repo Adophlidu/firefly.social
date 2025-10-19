7666c8eb7b95e9283c22a0e4d08dc85a6920e229
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2025-10-20 01:40:02 +0800

    fix: is_developement (#6424)

--- src/components/QueryClientProviders.tsx
+++ src/components/QueryClientProviders.tsx
@@ -6,13 +6,12 @@ import { ReactQueryStreamedHydration } from '@tanstack/react-query-next-experime
 import type { ReactNode } from 'react';
 
 import { queryClient } from '@/configs/queryClient.js';
-import { IS_DEVELOPMENT } from '@/constants/index.js';
 
 export function QueryClientProviders({ children }: { children: ReactNode }) {
     return (
         <QueryClientProvider client={queryClient}>
             <ReactQueryStreamedHydration>{children}</ReactQueryStreamedHydration>
-            {IS_DEVELOPMENT ? <ReactQueryDevtools client={queryClient} /> : null}
+            {process.env.NODE_ENV === 'development' ? <ReactQueryDevtools client={queryClient} /> : null}
         </QueryClientProvider>
     );
 }
