537f56141836d9d3d214780d47d0f633607f1969
guanbinrui <52657989+guanbinrui@users.noreply.github.com>
2025-11-04 13:43:01 +0800

    refactor: getAllPlatformProfileFromFirefly (#6606)

--- src/providers/firefly/getAllPlatformProfileFromFirefly.ts
+++ src/providers/firefly/getAllPlatformProfileFromFirefly.ts
@@ -2,47 +2,48 @@ import { isHex } from 'viem';
 
 import { NetworkType, Source } from '@/constants/enum.js';
 import { getAddressType } from '@/helpers/getAddressType.js';
-import { resolveValue } from '@/helpers/resolveValue.js';
 import { getAllRelatedProfileInfo } from '@/providers/firefly/getAllRelatedProfileInfo.js';
 import { type FireflyIdentity } from '@/providers/types/Firefly.js';
+
+function getQueryKey(identity: FireflyIdentity, forceHandle: boolean) {
+    switch (identity.source) {
+        case Source.Lens:
+            if (isHex(identity.id) && !forceHandle) return 'lensProfileId';
+            return 'lensHandle';
+        case Source.Farcaster:
+            return forceHandle ? 'farcasterUsername' : 'fid';
+        case Source.Twitter:
+            return /^\d+$/.test(identity.id) && !forceHandle ? 'twitterId' : 'twitterHandle';
+        case Source.Wallet:
+        case Source.WalletMix:
+            const addressType = getAddressType(identity.id, false);
+            switch (addressType) {
+                case NetworkType.Ethereum:
+                    return 'walletAddress';
+                case NetworkType.Solana:
+                    return 'solanaAddress';
+                default:
+                    return 'walletAddress';
+            }
+        case Source.Bsky:
+            if (identity.id.startsWith('did:plc:')) return 'bskyDid';
+            return 'bskyHandle';
+        case Source.Firefly:
+            return 'uid';
+        default:
+            return '';
+    }
+}
 
 export async function getAllPlatformProfileFromFirefly(
     identity: FireflyIdentity,
     isAuthRequired: boolean,
     forceHandle = false,
 ) {
-    const queryKey = resolveValue(() => {
-        switch (identity.source) {
-            case Source.Lens:
-                if (isHex(identity.id) && !forceHandle) return 'lensProfileId';
-                return 'lensHandle';
-            case Source.Farcaster:
-                return forceHandle ? 'farcasterUsername' : 'fid';
-            case Source.Twitter:
-                return /^\d+$/.test(identity.id) && !forceHandle ? 'twitterId' : 'twitterHandle';
-            case Source.Wallet:
-            case Source.WalletMix:
-                switch (getAddressType(identity.id, false)) {
-                    case NetworkType.Ethereum:
-                        return 'walletAddress';
-                    case NetworkType.Solana:
-                        return 'solanaAddress';
-                    default:
-                        return 'walletAddress';
-                }
-            case Source.Bsky:
-                if (identity.id.startsWith('did:plc:')) return 'bskyDid';
-                return 'bskyHandle';
-            case Source.Firefly:
-                return 'uid';
-            default:
-                return '';
-        }
-    });
-
+    const queryKey = getQueryKey(identity, forceHandle);
     return getAllRelatedProfileInfo(
         {
-            [`${queryKey}`]: identity.id,
+            [queryKey]: identity.id,
         },
         isAuthRequired,
     );
