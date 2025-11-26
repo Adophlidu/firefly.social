85290104bb00530a32c9c5e77da1e0de97582242
nuanyang233 <nuanyang233@gmail.com>
2025-11-26 19:18:16 +0800

    fix: bugfix for sparks page (#6860)

--- src/app/(normal)/sparks/page.tsx
+++ src/app/(normal)/sparks/page.tsx
@@ -2,15 +2,11 @@ import type { Metadata } from 'next';
 
 import { SparksModal } from '@/components/Sparks/SparksModal.js';
 import { createSparksMetadata } from '@/providers/firefly/metadata/createSparksMetadata.js';
-import type { NextPageProps } from '@/types/utility.js';
-
-interface Props extends NextPageProps<{ accountId: string }> {}
 
 export async function generateMetadata(): Promise<Metadata> {
     return createSparksMetadata();
 }
 
-export default async function SparksPage(props: Props) {
-    const { accountId } = await props.params;
-    return <SparksModal uid={accountId} />;
+export default async function SparksPage() {
+    return <SparksModal />;
 }
