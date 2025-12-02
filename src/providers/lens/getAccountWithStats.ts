35429de43e8dd5681de15c9a4d2b968143ea7aa6
dudu0506 <dudu@mask.io>
2025-12-02 19:41:14 +0800

    fix: lens account gq query (#6942)
    https://mask.atlassian.net/browse/FW-6186
    https://mask.atlassian.net/browse/FW-6187

--- src/providers/lens/getAccountWithStats.ts
+++ src/providers/lens/getAccountWithStats.ts
@@ -10,14 +10,14 @@ import {
 import { LENS_PRO_GROUP_ID } from '@/constants/lens.js';
 import { safeEvmAddress } from '@/helpers/safeEvmAddress.js';
 import { formatLensProfileV3 } from '@/providers/lens/formatLensProfile.js';
-import { lensClientHolder } from '@/providers/lens/LensClientHolder.js';
+import { getLensClient } from '@/providers/lens/getLensClient.js';
 import type { Profile } from '@/providers/types/SocialMedia.js';
 
 async function getAccountWithStats(
     accountRequest: AccountRequest,
     accountStatsRequest: AccountStatsRequest,
 ): Promise<Profile> {
-    const result = await lensClientHolder.client.urql.query<
+    const result = await getLensClient().urql.query<
         {
             account: Account;
             accountStats: AccountStats;
