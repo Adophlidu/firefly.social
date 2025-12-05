d4286854178799a2389994a3d56c30e44a513ed3
zz <232987290+ChiKiiCode@users.noreply.github.com>
2025-12-05 18:37:34 +0800

    fix: update searchToken response type to allow null values (#6999)

--- src/providers/firefly/worker/searchToken.ts
+++ src/providers/firefly/worker/searchToken.ts
@@ -8,7 +8,7 @@ import type { GetTokenOptions } from '@/providers/types/Firefly.js';
 import type { ResponseJson } from '@/types/utility.js';
 
 export async function searchToken(options: GetTokenOptions): Promise<CoinGeckoToken | null> {
-    const response = await fetchJson<ResponseJson<CoinGeckoToken>>(
+    const response = await fetchJson<ResponseJson<CoinGeckoToken | null>>(
         urlcat(FIREFLY_WORKER_HOST, '/token/search', options),
     );
     return resolveResponseData(response);
