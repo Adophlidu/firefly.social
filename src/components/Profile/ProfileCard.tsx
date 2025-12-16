061f99460ace652eb70e707d1a7f6e6e7af54608
dudu0506 <dudu@mask.io>
2025-12-16 12:44:28 +0800

    fix: refetch bsky profile in profile tippy (#7107)

--- src/components/Profile/ProfileCard.tsx
+++ src/components/Profile/ProfileCard.tsx
@@ -32,12 +32,12 @@ export const ProfileCard = memo<ProfileCardProps>(function ProfileCard({ identit
     const { id, source } = identity;
 
     const myProfile = useCurrentProfile(narrowToSocialSource(source));
-    const queryKey = [Source.Lens, Source.Twitter].includes(source)
+    const queryKey = [Source.Lens, Source.Twitter, Source.Bsky].includes(source)
         ? ['profile', source, id, 'tippy']
         : ['profile', source, id];
     const { data: profile, isLoading } = useQuery({
         queryKey,
-        staleTime: 1000 * 60 * 5, // 5 minutes
+        staleTime: 1000 * 60 * 10, // 10 minutes
         queryFn: async () => {
             if (!id || !source) return;
             const provider = resolveSocialMediaProvider(narrowToSocialSource(source));
