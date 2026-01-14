044d5e2518dee6002432378618905441ed49998b
nuanyang233 <nuanyang233@gmail.com>
2026-01-15 07:35:24 +0800

    fix: bugfix for not found layout (#7369)

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,12 +2,7 @@ import { msg } from '@lingui/core/macro';
 import { Trans } from '@lingui/react/macro';
 
 import { BaseNotFound } from '@/components/BaseNotFound.js';
-import { IfPathname } from '@/components/IfPathname.js';
 import { Link } from '@/components/Link.js';
-import { LinkCloud } from '@/components/LinkCloud.js';
-import { AsideSearchBar } from '@/components/Search/SearchBar.js';
-import { SuggestedChannels } from '@/components/SuggestedChannels/SuggestedChannels.js';
-import { SuggestedFollows } from '@/components/SuggestedFollows/SuggestedFollows.js';
 import { PageRoute } from '@/constants/enum.js';
 import { createPageTitleSSR } from '@/helpers/createPageTitle.js';
 import { createSiteMetadata } from '@/helpers/createSiteMetadata.js';
@@ -32,18 +27,6 @@ export default async function NotFound() {
                     <Trans>Back to home</Trans>
                 </Link>
             </BaseNotFound>
-            <aside className="sticky top-0 z-1 hidden h-full w-96 flex-col gap-4 px-4 md:min-w-[384px] lg:flex">
-                <IfPathname isNotOneOf={[PageRoute.Settings]}>
-                    <AsideSearchBar />
-                </IfPathname>
-                <div className="no-scrollbar flex flex-1 flex-col gap-4 overflow-auto">
-                    <IfPathname isNotOneOf={[PageRoute.Home]} exact>
-                        <SuggestedFollows />
-                        <SuggestedChannels />
-                    </IfPathname>
-                    <LinkCloud />
-                </div>
-            </aside>
         </>
     );
 }
--- src/components/Oembed/index.tsx
+++ src/components/Oembed/index.tsx
@@ -1,8 +1,6 @@
 import { memo } from 'react';
 
 import { Embed } from '@/components/Oembed/Embed.js';
-import { Player } from '@/components/Oembed/Player.js';
-import { isLinkMatchingHost } from '@/helpers/isLinkMatchingHost.js';
 import { type LinkDigested, type OpenGraph } from '@/types/og.js';
 
 interface OembedUIProps {
@@ -10,11 +8,7 @@ interface OembedUIProps {
 }
 
 const OembedUI = memo<OembedUIProps>(function OembedUI({ og }) {
-    return og.html ? (
-        <Player html={og.html} isSpotify={isLinkMatchingHost(og.url, 'open.spotify.com', false)} />
-    ) : (
-        <Embed og={og} />
-    );
+    return <Embed og={og} />;
 });
 
 export const OembedLayout = memo<{ data: LinkDigested }>(function OembedPayload(props) {
