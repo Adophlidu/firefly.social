fa0c65059dfc5b4f305ff8e6b649bd2de7357a5d
Wukong Sun <swkatmask@gmail.com>
2026-01-27 11:05:24 +0800

    chore(okx): fw-6705 update aggregator api endpoint to v6 (#7516)

--- src/providers/okx/getSupportedChains.ts
+++ src/providers/okx/getSupportedChains.ts
@@ -8,7 +8,7 @@ import { type SupportedChainResponse } from '@/providers/okx/types.js';
  * @docs https://www.okx.com/web3/build/docs/waas/dex-get-aggregator-supported-chains
  */
 export async function getSupportedChains() {
-    const url = urlcat(OKX_HOST, '/api/v5/dex/aggregator/supported/chain');
+    const url = urlcat(OKX_HOST, '/api/v6/dex/aggregator/supported/chain');
     const res = await fetchFromOKX<SupportedChainResponse>(url);
     return res.code === 0 ? res.data : undefined;
 }
