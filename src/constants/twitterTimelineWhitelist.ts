58ea2315a4aadba372d27b95cb5837df304223fc
LeifXu <167842553+LeifXu@users.noreply.github.com>
2026-01-27 09:43:03 +0800

    feat: expand Twitter timeline whitelist with additional UIDs (#7512)

--- src/constants/twitterTimelineWhitelist.ts
+++ src/constants/twitterTimelineWhitelist.ts
@@ -1,24 +1,117 @@
 /* cspell:disable */
 
 export const TWITTER_TIMELINE_WHITELIST_UID_SET: ReadonlySet<string> = new Set([
+    '1242864733', // x: @YourAirdropETH
+    '1256554211', // x: @sleepy0x13
+    '1281584302', // x: @EdisonZz798
+    '1305219755', // x: @EnHeng456
+    '1310798310', // x: @Xiaoniu6161
     '1314027998',
+    '1359867565', // x: @choc07_
+    '1366510449', // x: @Nazarick_eth
     '1426553307', // farcaster: @takanenohirose
     '1444316598', // Jun farcaster: junzhang
+    '1449711648', // x: @timigod
+    '1478815551', // x: @salmon_pi
+    '1507589147', // x: @cyrilxuq
+    '1509864609', // x: @Paris13Jeanne
+    '1541574285', // x: @SOU_BTC
     '1544180057', // lowenwen
+    '1552107477', // x: @RootDataCrypto
+    '1588661686', // x: @besting_crypto
     '1612040233', // loxia.eth
+    '1630574244', // x: @crypto___baby
     '1654026192', // Yazov farcaster: @antun0v1c
+    '1661880209', // x: @dcbuilder
+    '1663531755', // x: @Yiyixmb
+    '1669257114', // x: @sheldonbitmart
+    '1671899587', // x: @leonelzab
+    '1718950287', // x: @BigFIBO
+    '1747173366', // x: @PeterTran_CT
     '1753375837', // farcaster: @saitejak
+    '1766221193', // x: @iamyourchaos
+    '1769230676', // x: @BuildWithScroll
+    '1773891705', // x: @Amasa_io
+    '1789416233', // x: @setyamickala
+    '1797967569', // x: @CBD0x
+    '1802995609', // x: @C70889594
+    '1808353956', // x: @nr358
+    '1844981569', // x: @MelodAI_pro
+    '1849642598', // x: @CryptoPainter
+    '1862304447', // x: @pcobidco
+    '1869607949', // x: @dingyi
+    '1870672279', // x: @periclesukraine
+    '1871112690', // x: @JYdmnLFG
+    '1902565460', // x: @flock_io
     '1918273493', // suji
+    '1933944826', // x: @thecryptoskanda
+    '1946863584', // x: @MayGordon01
+    '1980518414', // x: @periclesrus
+    '2022467672', // x: @evilcos
+    '2043240447', // x: @y95277777
+    '2050947659', // x: @etherdrops_bot
+    '2058948481', // x: @goldshellminer
+    '2061884562', // x: @0xVeryBigOrange
+    '2103430109', // x: @DoctorMbitcoin
     '2103613615', // x: @LeifXu88998899
     '2171547191', // hongkongdoll
+    '2225314177', // x: @OdunHerif
     '2229461391', // farcaster: @kalel04.eth
     '2250087362', // danfinlay
+    '2268731476', // x: @Airdrop_Guard
+    '2270016012', // x: @hpcalls
+    '2279154564', // x: @Sea_Bitcoin
     '2283485284', // ddjking
+    '2307340943', // x: @taresky
+    '2317874135', // x: @vvxiaoyu8888
+    '2322681967', // x: @0xJamesXXX
+    '2378577611', // x: @btcfx295
+    '2420123160', // x: @SwapSpaceCo
+    '2435537586', // x: @_FORAB
     '2471908290', // songadaymann
+    '2477853854', // x: @shahrianazim6
     '2495874052', // vitalik.eth
+    '2516793631', // x: @metyacom
+    '2543328463', // x: @ZF_lab
+    '2589094628', // x: @hotbabyrebao
+    '2621795232', // x: @GMAgents_AI
+    '2681544482', // x: @paji_a
     '2686278913', // David Hoffman
+    '2694866951', // x: @nihaovand
+    '2715125910', // x: @colinwu
+    '2717951888', // x: @nathanisalive
+    '2754175789', // x: @WY_mask
+    '2779793097', // x: @bleuonbase
+    '2783022786', // x: @thefireflyapp
+    '2783573302', // x: @AdEx_Network
+    '2840459873', // x: @XXY177
+    '2851873888', // x: @0xCurva
+    '2898961290', // x: @PANews
+    '2903732421', // x: @MiaBleem
+    '2911696541', // x: @daice_0622
+    '2919954270', // x: @WORLD3_AI
+    '2934818180', // x: @bazingahappy
+    '2945975288', // x: @jason_chen998
+    '2957866417', // x: @MartinHo99999
+    '2959040849', // x: @SecondLiveReal
     '2974712182', // farcaster: @fdnxyz
+    '2981685375', // x: @gezhiwo_
+    '2992842484', // x: @EFDevcon
+    '2997907224', // x: @fugui8
     '3008110105',
+    '3021872985', // x: @ohyishi
+    '3063455235', // x: @smartek
+    '3172934359', // x: @hotpot_dao
+    '3182733530', // x: @0xdahua
     '3207396612', // farcaster: @kunyang
+    '3218370215', // x: @Elizabethofyou
     '3241455152',
+    '3261457205', // x: @Thienthien1305
+    '3306599453', // x: @MEJ50749
+    '3314049758', // x: @sukie234
+    '3333097584', // x: @lucianlampdefi
+    '3348291805', // x: @KevinSvenson_
+    '3353723555', // x: @off_thetarget
+    '3361761077', // x: @0xKirara
+    '3361871179', // x: @akibablade
 ]);
