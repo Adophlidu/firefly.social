12f67c72ddccc7459aae9ccd99c1b8dd08ab29c3
dudu0506 <dudu@mask.io>
2026-01-29 10:44:15 +0800

    fix: format usd value in bets profile page (#7525)
    * fix: format usd value in bets profile page
    
    https://mask.atlassian.net/browse/FW-6738
    
    * fix: trans
    
    * chore: code maintenance [bot]
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- src/components/Prediction/PredictionTradeTimelineItem.tsx
+++ src/components/Prediction/PredictionTradeTimelineItem.tsx
@@ -20,13 +20,10 @@ export function PredictionTradeTimelineItem({ trade, platform }: PredictionTrade
     const walletAddress = trade.proxyWallet || trade.wallet;
     const addressName = formatAddress(walletAddress, 4);
     const avatarUrl = trade.displayInfo?.avatarUrl || getStampAvatarByProfileId(Source.Wallet, walletAddress);
-    const txType = {
-        buy: <Trans>Bought</Trans>,
-        sell: <Trans>Sold</Trans>,
-    }[trade.side];
     const betsProfileUrl = RouteResolver.betsProfile(walletAddress, { platform });
     const marketTitle =
         platform === PredictionPlatform.Polymarket ? trade.rawData?.groupItemTitle || trade.title : trade.title;
+    const usdValue = multipliedBy(trade.size, trade.price).toNumber();
 
     return (
         <div className="flex items-center gap-2 border-b border-line px-4 py-3">
@@ -34,21 +31,39 @@ export function PredictionTradeTimelineItem({ trade, platform }: PredictionTrade
                 <Avatar src={avatarUrl} size={36} alt={walletAddress} />
             </Link>
             <div className="text-sm font-medium text-second">
-                <Trans>
-                    <Link href={betsProfileUrl} target="_blank" className="text-main hover:underline">
-                        {trade.displayInfo?.ensHandle || addressName}
-                    </Link>{' '}
-                    {txType}{' '}
-                    <span
-                        className={classNames(
-                            'font-semibold',
-                            trade.outcomeIndex === 0 ? 'text-success' : 'text-danger',
-                        )}
-                    >{`${toFixedTrimmed(+trade.size, 2)} ${trade.outcome}`}</span>{' '}
-                    for <span className="text-main">{marketTitle}</span> at{' '}
-                    <span className="text-main">{`${toFixedTrimmed(+trade.price * 100, 1)}¢`}</span>
-                    {` ($${toFixedTrimmed(multipliedBy(trade.size, trade.price).toNumber(), 2)})`}
-                </Trans>
+                {trade.side === 'buy' ? (
+                    <Trans>
+                        <Link href={betsProfileUrl} target="_blank" className="text-main hover:underline">
+                            {trade.displayInfo?.ensHandle || addressName}
+                        </Link>{' '}
+                        Bought{' '}
+                        <span
+                            className={classNames(
+                                'font-semibold',
+                                trade.outcomeIndex === 0 ? 'text-success' : 'text-danger',
+                            )}
+                        >{`${toFixedTrimmed(+trade.size, 2)} ${trade.outcome}`}</span>{' '}
+                        for <span className="text-main">{marketTitle}</span> at{' '}
+                        <span className="text-main">{`${toFixedTrimmed(+trade.price * 100, 1)}¢`}</span>
+                        {` (${usdValue < 0.01 ? '< $0.01' : `$${toFixedTrimmed(usdValue, 2)}`})`}
+                    </Trans>
+                ) : (
+                    <Trans>
+                        <Link href={betsProfileUrl} target="_blank" className="text-main hover:underline">
+                            {trade.displayInfo?.ensHandle || addressName}
+                        </Link>{' '}
+                        Sold{' '}
+                        <span
+                            className={classNames(
+                                'font-semibold',
+                                trade.outcomeIndex === 0 ? 'text-success' : 'text-danger',
+                            )}
+                        >{`${toFixedTrimmed(+trade.size, 2)} ${trade.outcome}`}</span>{' '}
+                        for <span className="text-main">{marketTitle}</span> at{' '}
+                        <span className="text-main">{`${toFixedTrimmed(+trade.price * 100, 1)}¢`}</span>
+                        {` (${usdValue < 0.01 ? '< $0.01' : `$${toFixedTrimmed(usdValue, 2)}`})`}
+                    </Trans>
+                )}
             </div>
         </div>
     );
