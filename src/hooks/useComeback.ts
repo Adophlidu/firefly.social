389bcf461a82378b26bfbb3fc4355d7aced3727d
dudu0506 <dudu@mask.io>
2026-02-10 14:51:25 +0800

    fix: come back (#7655)

--- src/hooks/useComeback.ts
+++ src/hooks/useComeback.ts
@@ -1,35 +1,25 @@
 'use client';
 
-import { first } from 'lodash-es';
 import { useCallback } from 'react';
 
 import { PageRoute } from '@/constants/enum.js';
 import { useRouter } from '@/esm/navigation.js';
-import { logger } from '@/libs/Logger.js';
+import { useIsLogin } from '@/hooks/useIsLogin.js';
 import { useGlobalState } from '@/store/useGlobalStore.js';
 
-export function useComeBack(path = PageRoute.Home) {
+export function useComeBack(path?: PageRoute) {
     const router = useRouter();
+    const isLogin = useIsLogin();
+
+    const defaultPath = path || (isLogin ? PageRoute.FollowingPosts : PageRoute.DiscoverPosts);
 
     return useCallback(() => {
         const routeChanged = useGlobalState.getState().routeChanged;
         if (!routeChanged) {
-            router.push(path);
+            router.push(defaultPath);
             return;
         }
 
-        // Check if the page was reloaded or navigated to directly, and if so, navigate to the specified path instead of going back
-        try {
-            const navigationEntries = performance.getEntriesByType('navigation');
-            const firstEntry = first(navigationEntries) as PerformanceNavigationTiming | undefined;
-            if (firstEntry?.type === 'reload' || firstEntry?.type === 'navigate') {
-                router.push(path);
-                return;
-            }
-        } catch (error) {
-            logger.error('Error checking navigation type:', error);
-        }
-
         router.back();
-    }, [path, router]);
+    }, [defaultPath, router]);
 }
