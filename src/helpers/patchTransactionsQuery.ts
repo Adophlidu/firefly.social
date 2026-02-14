dca5ae2f209d738b5c41b57dd53463092131b428
Wukong Sun <swkatmask@gmail.com>
2026-02-14 11:40:43 +0800

    fix: fw-6975 update like indicator not showing in wallet profile (#7716)

--- src/helpers/patchTransactionsQuery.ts
+++ src/helpers/patchTransactionsQuery.ts
@@ -15,32 +15,45 @@ export function patchTransactionsQuery<
             ? WritableDraft<NFTFeedV3>
             : never,
 >(source: T, updater?: (item: D) => void, filter?: (item: D) => boolean): void {
+    const patcher = (old?: { pages: Array<{ data: TransactionsItem[] }> }) => {
+        if (!old) return old;
+
+        return produce(old, (draft) => {
+            draft.pages.forEach((page) => {
+                if (!page.data?.length) return;
+
+                if (filter) {
+                    page.data = page.data.filter((item) => {
+                        return item.source !== source || filter(item.data as D);
+                    });
+                } else {
+                    page.data.forEach((item) => {
+                        if (item.source === source) {
+                            updater?.(item.data as D);
+                        }
+                    });
+                }
+            });
+        });
+    };
+
+    // Update queries for home page transactions
     queryClient.setQueriesData<{
         pages: Array<{ data: TransactionsItem[] }>;
     }>(
         {
             queryKey: ['transactions'],
         },
-        (old) => {
-            if (!old) return old;
+        patcher,
+    );
 
-            return produce(old, (draft) => {
-                draft.pages.forEach((page) => {
-                    if (!page.data?.length) return;
-
-                    if (filter) {
-                        page.data = page.data.filter((item) => {
-                            return item.source !== source || filter(item.data as D);
-                        });
-                    } else {
-                        page.data.forEach((item) => {
-                            if (item.source === source) {
-                                updater?.(item.data as D);
-                            }
-                        });
-                    }
-                });
-            });
+    // Update queries for profile transactions
+    queryClient.setQueriesData<{
+        pages: Array<{ data: TransactionsItem[] }>;
+    }>(
+        {
+            queryKey: ['profile', 'transactions'],
         },
+        patcher,
     );
 }
