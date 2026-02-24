fc7ada7187f1b65a51deb9e616de642e3fd29852
Wukong Sun <swkatmask@gmail.com>
2026-02-24 16:15:56 +0800

    fix: fw-6860 improve skeleton loader layout in PredictionProfilesCard (#7729)

--- src/components/Prediction/PredictionProfilesCard.tsx
+++ src/components/Prediction/PredictionProfilesCard.tsx
@@ -25,18 +25,18 @@ export function PredictionProfilesCard({ address }: PredictionProfilesCardProps)
             <div className="flex animate-pulse justify-evenly gap-3 rounded-xl bg-primaryBottom p-3">
                 <div className="flex flex-1 items-center gap-2 text-main">
                     <div className="size-8 shrink-0 rounded-full bg-third" />
-                    <div className="flex h-9 flex-col justify-between">
-                        <div className="h-4 w-[96px] shrink-0 rounded-[4px] bg-third" />
-                        <div className="flex h-4 w-[64px] items-center rounded-[4px] bg-third" />
+                    <div className="flex h-9 flex-1 flex-col justify-between">
+                        <div className="h-4 w-full max-w-[96px] shrink-0 rounded-[4px] bg-third" />
+                        <div className="flex h-4 w-1/2 max-w-[64px] items-center rounded-[4px] bg-third" />
                     </div>
                 </div>
                 <div className="flex h-9 flex-1 flex-col items-end justify-between">
-                    <div className="h-4 w-[64px] shrink-0 rounded-[4px] bg-third" />
-                    <div className="ml-auto flex h-4 w-[128px] items-center rounded-[4px] bg-third" />
+                    <div className="h-4 w-1/2 max-w-[64px] shrink-0 rounded-[4px] bg-third" />
+                    <div className="ml-auto flex h-4 w-full max-w-[128px] items-center rounded-[4px] bg-third" />
                 </div>
                 <div className="flex h-9 flex-1 flex-col items-end justify-between">
-                    <div className="h-4 w-[64px] shrink-0 rounded-[4px] bg-third text-sm font-semibold" />
-                    <div className="ml-auto flex h-4 w-[128px] items-center rounded-[4px] bg-third" />
+                    <div className="h-4 w-1/2 max-w-[64px] shrink-0 rounded-[4px] bg-third text-sm font-semibold" />
+                    <div className="ml-auto flex h-4 w-full max-w-[128px] items-center rounded-[4px] bg-third" />
                 </div>
             </div>
         );
