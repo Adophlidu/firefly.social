acdb74b160f273fa89daadcbaf47a8e199a42d98
nuanyang233 <nuanyang233@gmail.com>
2026-02-26 11:35:30 +0800

    fix: remove url protocol-residue (#7747)
    * fix: remove url protocol-residue
    
    * chore: code maintenance [bot]
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- src/helpers/removeAtEnd.ts
+++ src/helpers/removeAtEnd.ts
@@ -6,11 +6,11 @@ export function removeAtEnd(content: string, fragment: string): string {
     content = content.trimEnd();
 
     const bare = stripProtocol(fragment);
-    const indexOfUrl = content.indexOf(bare);
-    if (indexOfUrl === -1) return content;
-
-    if (indexOfUrl === content.length - bare.length) {
-        return content.replace(bare, '');
+    if (content.endsWith(bare)) {
+        return content
+            .slice(0, content.lastIndexOf(bare))
+            .replace(/https?:\/\/$/, '')
+            .trimEnd();
     }
 
     return content;
