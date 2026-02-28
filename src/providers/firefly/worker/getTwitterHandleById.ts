0c51e6f70408f16596ab0f782c60208648121d05
Wukong Sun <swkatmask@gmail.com>
2026-02-28 15:54:57 +0800

    fix(providers): fw-6997 add cache lookup for twitter handle retrieval (#7788)

--- src/providers/firefly/worker/getTwitterHandleById.ts
+++ src/providers/firefly/worker/getTwitterHandleById.ts
@@ -1,8 +1,11 @@
 import urlcat from 'urlcat';
 
+import { queryClient } from '@/configs/queryClient.js';
+import { Source } from '@/constants/enum.js';
 import { FIREFLY_WORKER_HOST } from '@/constants/static.js';
 import { fetchJson } from '@/helpers/fetchJson.js';
 import { isNumericalProfileId } from '@/helpers/isNumericalProfileId.js';
+import type { Profile } from '@/providers/types/SocialMedia.js';
 import { type ResponseJson } from '@/types/utility.js';
 
 type GetTwitterHandleByIdResponse = ResponseJson<{
@@ -12,6 +15,11 @@ type GetTwitterHandleByIdResponse = ResponseJson<{
 export async function getTwitterHandleById(id: string) {
     // id is supposed to be numerical, but just in case
     if (!isNumericalProfileId(id)) return id;
+    // /x-identity will might fail.
+    const profile = queryClient.getQueriesData<Profile>({ queryKey: ['profile', Source.Twitter] }).find((x) => {
+        x[1]?.profileId === id;
+    });
+    if (profile?.[1]?.handle) return profile[1].handle;
     const response = await fetchJson<GetTwitterHandleByIdResponse>(
         urlcat(FIREFLY_WORKER_HOST, `/x-identity`, {
             id,
