421991afcc611448e5a6b196b3a7eea6ce2a8706
nuanyang233 <nuanyang233@gmail.com>
2026-03-02 18:31:58 +0800

    chore: rm duplicate post header (#7800)

--- src/app/(normal)/post/[source]/[id]/(detail)/error.tsx
+++ src/app/(normal)/post/[source]/[id]/(detail)/error.tsx
@@ -8,7 +8,7 @@ import { SearchType } from '@/constants/enum.js';
 import { TweetUnavailableError } from '@/constants/error.js';
 import { enqueueWarningMessage } from '@/helpers/enqueueMessage.js';
 
-export default function Error({ error, reset }: { error: Error; reset: () => void }) {
+export default function Error({ error }: { error: Error }) {
     useEffect(() => {
         if (error instanceof TweetUnavailableError && error.message) {
             enqueueWarningMessage(error.message);
@@ -18,7 +18,6 @@ export default function Error({ error, reset }: { error: Error; reset: () => voi
     if (error instanceof TweetUnavailableError) {
         return (
             <NotFound
-                backText={<Trans>Post details</Trans>}
                 text={<Trans>Post could not be found.</Trans>}
                 search={{ text: <Trans>Search post</Trans>, searchText: '', searchType: SearchType.Posts }}
             />
