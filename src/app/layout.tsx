508da84fa7cc37510cde6ed3f218f83cc847405f
dudu0506 <dudu@mask.io>
2026-03-20 16:50:02 +0800

    fix: rm referrer header in global fetch (#7960)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,7 +57,8 @@ export default async function RootLayout({ children }: { children: ReactNode }) 
     return (
         <html lang={rootLocale ? resolveLocale(rootLocale) : Locale.en} className={rootClass}>
             <head>
-                <meta name="referrer" content="no-referrer-when-downgrade" />
+                {/* Videos from twitter can not play on firefly, so we add this to fix */}
+                <meta name="referrer" content="no-referrer" />
                 <meta name="theme-color" content="#ffffff" />
                 <meta name="googlebot" content="notranslate" />
                 {IS_PRODUCTION ? null : <meta name="robots" content="noindex, nofollow" />}
