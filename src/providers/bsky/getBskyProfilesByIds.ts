68e703f12d967cc9afb73c70ab6b6db3d93865ad
nuanyang233 <nuanyang233@gmail.com>
2026-03-20 15:07:55 +0800

    feat: handle empty profile ID list in getBskyProfilesByIds function (#7957)

--- src/providers/bsky/getBskyProfilesByIds.ts
+++ src/providers/bsky/getBskyProfilesByIds.ts
@@ -1,9 +1,11 @@
+import { EMPTY_LIST } from '@/constants/static.js';
 import { formatBskyProfile } from '@/providers/bsky/formatBskyProfile.js';
 import { resolveBskyResponseData } from '@/providers/bsky/resolveBskyResponseData.js';
 import { bskySessionHolder } from '@/providers/bsky/SessionHolder.js';
 import { type Profile } from '@/providers/types/SocialMedia.js';
 
 export async function getBskyProfilesByIds(ids: string[], signal?: AbortSignal): Promise<Profile[]> {
+    if (!ids.length) return EMPTY_LIST;
     const response = await bskySessionHolder.agent.getProfiles({ actors: ids }, { signal });
     const data = resolveBskyResponseData(response, `Failed to get profiles ids = ${ids.join(',')}.`);
     return data.profiles.map((profile) => formatBskyProfile(profile));
