1493facdb445564b658d0ef4400d5c0226a507f5
LeifXu <167842553+LeifXu@users.noreply.github.com>
2026-03-27 16:07:35 +1030

    fix: include path prefix in rewrite destinations to prevent redirect loop (#8045)

--- .next-config/rewrites.config.ts
+++ .next-config/rewrites.config.ts
@@ -37,8 +37,8 @@ export const rewritesConfig: NonNullable<NextConfig['rewrites']> = async () => {
 
     for (const [path, baseUrl] of routes) {
         rules.push(
-            { source: path, destination: `${baseUrl}/` },
-            { source: `${path}/:path*`, destination: `${baseUrl}/:path*` },
+            { source: path, destination: `${baseUrl}${path}/` },
+            { source: `${path}/:path*`, destination: `${baseUrl}${path}/:path*` },
         );
     }
 
