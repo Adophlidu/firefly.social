1cc1f22d9d07a490b571c9feb3f1ed6c1ab13a28
Wukong Sun <swkatmask@gmail.com>
2026-04-09 13:49:03 +0800

    fix: fw-7270 add mystery-box intercepting route in event group (#8195)

---
+++ apps/web/src/app/[locale]/(event)/@modal/(.)mystery-box/page.tsx
@@ -0,0 +1,8 @@
+import { MysteryBoxModal } from '@/components/MysteryBox/MysteryBoxModal.js';
+
+export const dynamic = 'force-dynamic';
+export const dynamicParams = true;
+
+export default function MysteryBoxModalPage() {
+    return <MysteryBoxModal />;
+}
