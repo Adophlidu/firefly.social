0fa5355f8621bde058385ebffc436eb10244248d
dudu0506 <dudu@mask.io>
2026-06-05 13:43:15 +0800

    fix: fifa og (#8970)

--- apps/web/src/app/[locale]/(event)/event/fifa/page.tsx
+++ apps/web/src/app/[locale]/(event)/event/fifa/page.tsx
@@ -1,12 +1,58 @@
 import type { Metadata } from 'next';
 
 import { WorldCupModal } from '@/components/WorldCup/WorldCupModal.js';
-import { createEventMetadata } from '@/providers/firefly/metadata/createEventMetadata.js';
 
 export const dynamic = 'force-dynamic';
 
 export async function generateMetadata(): Promise<Metadata> {
-    return createEventMetadata('fifa', '/event/fifa');
+    return {
+        metadataBase: 'https://firefly.social/',
+        title: 'FIFA Prediction Festival',
+        itunes: {
+            appId: '1640183078',
+        },
+        description:
+            'Back your favorite team on Firefly and turn your soccer insights into real rewards with a massive $25,000 prize pool!',
+        openGraph: {
+            type: 'website',
+            url: 'https://firefly.social/event/fifa',
+            title: 'FIFA Prediction Festival',
+            description:
+                'Back your favorite team on Firefly and turn your soccer insights into real rewards with a massive $25,000 prize pool!',
+            images: ['https://media.firefly.land/uploads/b38621e8-5869-449f-bb65-7f7a63fd115c.png'],
+        },
+        twitter: {
+            card: 'summary_large_image',
+            title: 'FIFA Prediction Festival',
+            description:
+                'Back your favorite team on Firefly and turn your soccer insights into real rewards with a massive $25,000 prize pool!',
+            images: ['https://media.firefly.land/uploads/b38621e8-5869-449f-bb65-7f7a63fd115c.png'],
+        },
+        manifest: '/site.webmanifest',
+        icons: [
+            {
+                url: '/android-chrome-192x192.png',
+                sizes: '192x192',
+                type: 'image/png',
+            },
+            {
+                url: '/android-chrome-384x384.png',
+                sizes: '384x384',
+                type: 'image/png',
+            },
+            {
+                rel: 'icon',
+                url: '/favicon.ico',
+            },
+            {
+                rel: 'apple-touch-icon',
+                url: '/apple-touch-icon.png',
+            },
+        ],
+        alternates: {
+            canonical: 'https://firefly.social/event/fifa',
+        },
+    };
 }
 
 export default function Page() {
