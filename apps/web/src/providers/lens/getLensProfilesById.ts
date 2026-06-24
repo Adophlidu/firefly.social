858050ea92996cbe7825b018cd0b21cc9f7641be
dudu0506 <dudu@mask.io>
2026-06-24 15:37:01 +0800

    fix(web): fetch lens profiles by ids (#9203)
    https://mask.atlassian.net/browse/FW-7807

--- apps/web/src/providers/lens/getLensProfilesById.ts
+++ apps/web/src/providers/lens/getLensProfilesById.ts
@@ -4,16 +4,35 @@ import { safeEvmAddress } from '@/helpers/safeEvmAddress.js';
 import { ensureLensResult } from '@/providers/lens/ensureLensResult.js';
 import { formatLensProfileV3 } from '@/providers/lens/formatLensProfile.js';
 import { getLensClient } from '@/providers/lens/getLensClient.js';
-import type { Notification, Profile } from '@/providers/types/SocialMedia.js';
+import type { Profile } from '@/providers/types/SocialMedia.js';
+
+// Maximum number of profiles to fetch in each request
+const MAX_COUNT_PER_REQUEST = 50;
 
 export async function getLensProfilesByIds(ids: string[]): Promise<Profile[]> {
     if (!ids.length) return [];
-    const result = await ensureLensResult(
-        fetchAccountsBulk(getLensClient(), {
-            addresses: ids.map(safeEvmAddress),
-        }),
-    );
-    const profiles = result.map(formatLensProfileV3);
 
-    return profiles;
+    const chunks: string[][] = [];
+    while (ids.length) {
+        const chunk = ids.splice(0, MAX_COUNT_PER_REQUEST);
+        chunks.push(chunk);
+    }
+
+    const results = await Promise.allSettled(
+        chunks.map(async (chunk) =>
+            ensureLensResult(
+                fetchAccountsBulk(getLensClient(), {
+                    addresses: chunk.map((id) => safeEvmAddress(id)),
+                }),
+            ),
+        ),
+    );
+
+    return results.flatMap((result) => {
+        if (result.status === 'fulfilled') {
+            return result.value.map(formatLensProfileV3);
+        }
+
+        return [];
+    });
 }
