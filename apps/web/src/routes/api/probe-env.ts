5d575e542d369c36166918a211d197b1707e9277
dudu0506 <dudu@mask.io>
2026-07-28 16:56:54 +0800

    chore: remove env probe route

--- apps/web/src/routes/api/probe-env.ts
+++
@@ -1,5 +0,0 @@
-import type { ApiContext } from '@dimensiondev/ssr';
-
-export function GET({ env }: ApiContext) {
-    return Response.json({ envKeys: env ? Object.keys(env) : null, hasAssets: Boolean(env && (env as { ASSETS?: unknown }).ASSETS) });
-}
